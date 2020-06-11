337e55c3a2844c2747fb4bd1c1c93281225d3119
JrainLau <jrainlau@163.com>
2020-06-11 19:14:20 +0800

    feat: support reverse

--- test/unit/steps/demo.test.js
+++ test/unit/steps/demo.test.js
@@ -3,7 +3,7 @@ import { mount } from '@vue/test-utils';
 // unit test for component in examples.
 describe('Steps', () => {
   it('base demo works fine', () => {
-    const demo = require('@/examples/steps/demos/base.vue');
+    const demo = require('@/examples/steps/demos/horizontal.vue');
     const wrapper = mount(demo);
     expect(wrapper.element).toMatchSnapshot();
   });
