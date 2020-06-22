2e44734dcadc86f836449c5c320f098c98fcd47f
qinmudi <qinmudi@139.com>
2020-06-22 16:22:06 +0800

    feat(menu): submenu active for t-default-menu

--- examples/menu/demos/side-menu-dropdown.vue
+++ examples/menu/demos/side-menu-dropdown.vue
@@ -1,8 +1,13 @@
 <template>
-  <t-menu theme="light" active="item2" height="550px">
+  <t-menu theme="light" active="2-1" height="550px">
     <img slot="logo" class="t-menu__logo" src="https://main.qcloudimg.com/raw/9fe1217de2bd7eb623f70648a046e341/head-logo.png" alt="logo">
     <t-menu-item name="item1">菜单内容一</t-menu-item>
-    <t-menu-item name="item2">已选内容</t-menu-item>
+    <t-submenu name="2">
+      <span slot="title">侧边内容二</span>
+      <t-menu-item name="2-1">菜单内容一</t-menu-item>
+      <t-menu-item name="2-2">菜单内容二</t-menu-item>
+      <t-menu-item name="2-3">菜单内容三</t-menu-item>
+    </t-submenu>
     <t-menu-item name="item3">菜单内容二</t-menu-item>
     <t-menu-item name="item4" :disabled="true">菜单内容三</t-menu-item>
     <div slot="options">
