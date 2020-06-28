fd19991c7f34efb0deaa2cb457f15cb9d87af59d
qinmudi <qinmudi@139.com>
2020-06-28 19:49:56 +0800

    feat(menu): add collapsed for menu

--- examples/menu/demos/side-menu-dropdown.vue
+++ examples/menu/demos/side-menu-dropdown.vue
@@ -1,19 +1,37 @@
 <template>
   <t-menu theme="light" active="2-1" height="550px">
     <img slot="logo" class="t-menu__logo" src="https://main.qcloudimg.com/raw/9fe1217de2bd7eb623f70648a046e341/head-logo.png" alt="logo">
-    <t-menu-item name="item1">菜单内容一</t-menu-item>
+    <t-menu-item name="item1">
+      <t-icon slot="icon" icon="user"/>菜单内容一
+    </t-menu-item>
     <t-submenu name="2">
+      <t-icon slot="icon" icon="user" />
       <span slot="title">侧边内容二</span>
       <t-menu-item name="2-1">菜单内容一</t-menu-item>
       <t-menu-item name="2-2">菜单内容二</t-menu-item>
       <t-menu-item name="2-3">菜单内容三</t-menu-item>
     </t-submenu>
-    <t-menu-item name="item3">菜单内容二</t-menu-item>
-    <t-menu-item name="item4" :disabled="true">菜单内容三</t-menu-item>
+    <t-menu-item name="item3"><t-icon slot="icon" icon="user" />菜单内容二</t-menu-item>
+    <t-menu-item name="item4" :disabled="disabled">
+      <t-icon slot="icon" icon="user" />菜单内容三
+    </t-menu-item>
     <div slot="options">
-      <a href="javascript:;"><i class="t-icon t-icon-demo"></i></a>
-      <a href="javascript:;"><i class="t-icon t-icon-demo"></i></a>
-      <a href="javascript:;"><i class="t-icon t-icon-demo"></i></a>
+      <t-icon icon="user" @click="changeCollapsed" />
     </div>
   </t-menu>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      disabled: true,
+    };
+  },
+  methods: {
+    changeCollapsed() {
+      this.collapsed = !this.collapsed;
+    },
+  },
+};
+</script>
