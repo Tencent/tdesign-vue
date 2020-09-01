50eaffe563020119cb4e89ed2c905e33efac7b24
gnauhca <576073927@qq.com>
2020-09-01 19:04:34 +0800

    feat(button): add shape

--- src/button/const.ts
+++ src/button/const.ts
@@ -2,7 +2,10 @@ export const THEME_LIST = ['line', 'primary', 'dashed', 'warning', 'warning-line
 
 export const SIZE_LIST = ['large', 'default', 'small'];
 
+export const SHAPE_LIST = ['default', 'round', 'circle'];
+
 export default {
   THEME_LIST,
   SIZE_LIST,
+  SHAPE_LIST,
 };
