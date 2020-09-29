80255f94c085c02681040996f08846fdd0d43a48
mixiaola <fezhangnan@sina.com>
2020-09-29 15:54:36 +0800

    feat: add tooltip

---
+++ examples/tooltip/demos/customize.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="tooltip">
+    <t-tooltip content="文字提示仅展示文本内容" :overlayStyle="{ background: '#35CD35' }">
+      <t-button>改变背景颜色</t-button>
+    </t-tooltip>
+    <t-tooltip content="文字提示仅展示文本内容" overlayClassName="custom-tooltip">
+      <t-button>改变文字颜色</t-button>
+    </t-tooltip>
+    <t-tooltip content="文字提示仅展示文本内容" :overlayStyle="{ background: '#FF3E00', color: '#FFAA00' }">
+      <t-button>支持class和style方式改变样式</t-button>
+    </t-tooltip>
+  </div>
+</template>
+<style>
+.custom-tooltip {
+	color: #0052D9 !important;
+}
+</style>
