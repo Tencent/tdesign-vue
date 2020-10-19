4e2deeb68876864516b0c31bae168c6703153336
mixiaola <fezhangnan@sina.com>
2020-10-19 15:39:54 +0800

    feat: 更新tooltip api

--- examples/tooltip/demos/customize.vue
+++ examples/tooltip/demos/customize.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="tooltip">
-    <t-tooltip content="文字提示仅展示文本内容" :overlayStyle="{ background: '#35CD35' }">
-      <t-button>改变背景颜色</t-button>
+    <t-tooltip content="文字提示仅展示文本内容" theme="Tblue" :visibleArrow="true">
+      <t-button>Tblue</t-button>
     </t-tooltip>
-    <t-tooltip content="文字提示仅展示文本内容" overlayClassName="custom-tooltip">
-      <t-button>改变文字颜色</t-button>
+    <t-tooltip content="文字提示仅展示文本" theme="Tgreen" :visibleArrow="true">
+      <t-button>Tgreen</t-button>
     </t-tooltip>
-    <t-tooltip content="文字提示仅展示文本内容" :overlayStyle="{ background: '#FF3E00', color: '#FFAA00' }">
-      <t-button>支持class和style方式改变样式</t-button>
+    <t-tooltip content="文字提示仅展示文本内容" theme="Tred" :visibleArrow="true">
+      <t-button>Tred</t-button>
+    </t-tooltip>
+    <t-tooltip content="文字提示仅展示文本内容" theme="Torange" :visibleArrow="true">
+      <t-button>Torange</t-button>
+    </t-tooltip>
+    <t-tooltip content="文字提示仅展示文本内容" theme="#2db7f5" :visibleArrow="true">
+      <t-button>自定义主题颜色</t-button>
     </t-tooltip>
   </div>
 </template>
 <style>
-.custom-tooltip {
-	color: #0052D9 !important;
+.tooltip {
+  display: flex;
+}
+.tooltip .t-button {
+  margin-right: 10px;
 }
 </style>
