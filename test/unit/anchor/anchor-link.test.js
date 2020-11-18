020267f129f438f2c2add8b8a1ca96908ec5f042
zwingz <zhengzwing@gmail.com>
2020-11-18 16:20:58 +0800

    feat(anchor): [WIP] anchor组件开发

---
+++ test/unit/anchor/anchor-link.test.js
@@ -0,0 +1,89 @@
+import { mount } from '@vue/test-utils';
+import Vue from 'vue';
+import { AnchorItem } from '@/src/anchor/index.ts';
+import { omit } from 'lodash';
+
+const defaultProvide = {
+  tAnchor: {
+    active: '',
+    handleScrollTo: () => {
+      //
+    },
+    registerLink: () => {
+      //
+    },
+    unregisterLink: () => {
+      //
+    },
+    handleLinkClick: () => {
+      //
+    },
+  },
+};
+
+// every component needs four parts: props/events/slots/functions.
+describe('AnchorItem', () => {
+  // test props api
+  describe('test lifecycle', () => {
+    it('should render props correctly', async () => {
+      const provide = {
+        active: '',
+        handleScrollTo: jest.fn(),
+        registerLink: jest.fn(),
+        unregisterLink: jest.fn(),
+        handleLinkClick: jest.fn(),
+      };
+      const props = {
+        href: '#test',
+        title: 'this is title',
+        target: '_blank',
+      };
+      const wrapper = mount({
+        provide: {
+          tAnchor: provide,
+        },
+        data() {
+          return props;
+        },
+        render() {
+          return <AnchorItem href={this.href} title={this.title} target={this.target}/>;
+        },
+      });
+      const a = wrapper.find('a');
+      expect(a.element.href).toEqual(location.href + props.href);
+      expect(a.element.title).toEqual(props.title);
+      expect(a.element.target).toEqual(props.target);
+      expect(a.element.textContent).toEqual(props.title);
+      expect(provide.registerLink).toBeCalledWith(props.href);
+      a.trigger('click');
+      expect(provide.handleScrollTo).toBeCalledWith(props.href);
+      expect(provide.handleLinkClick).toBeCalledWith(expect.any(MouseEvent), omit(props, 'target'));
+      wrapper.setData({
+        href: '#test-b',
+      });
+      await Vue.nextTick();
+      expect(provide.unregisterLink).toBeCalledWith(props.href);
+      expect(provide.registerLink).toBeCalledWith('#test-b');
+    });
+  });
+
+
+  // test slots
+  describe('<slot>', () => {
+    it('test slot', () => {
+      const wrapper = mount(AnchorItem, {
+        provide: defaultProvide,
+        propsData: {
+          href: '#a',
+        },
+        slots: {
+          title: '<div id="title"> this is title slot </div>',
+          default: '<div id="default"> this is children content </div>',
+        },
+      });
+      expect(wrapper.find('a').attributes('title')).toEqual('');
+      expect(wrapper.find('#title').element.outerHTML).toMatchSnapshot();
+      expect(wrapper.find('#default').element.outerHTML).toMatchSnapshot();
+    });
+  });
+});
