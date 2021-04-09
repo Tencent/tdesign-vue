15dff66a204881fd91d355a238238ee33b0b6197
gnauhca <576073927@qq.com>
2021-04-09 17:41:32 +0800

    style(button): button 视觉走查问题修复

--- examples/button/demos/shape.vue
+++ examples/button/demos/shape.vue
@@ -3,26 +3,34 @@
     <div class="tdesign-demo-block">
       <div class="item"><t-button shape="square" variant="base">主按钮</t-button></div>
       <div class="item"><t-button shape="round" variant="base">主按钮</t-button></div>
-      <div class="item"><t-button shape="circle" variant="base">i</t-button></div>
+      <div class="item"><t-button shape="circle" variant="base"><t-icon-calendar slot="icon"/></t-button></div>
     </div>
     <div class="tdesign-demo-block">
       <div class="item"><t-button shape="square" variant="outline">次要按钮</t-button></div>
       <div class="item"><t-button shape="round" variant="outline">次要按钮</t-button></div>
-      <div class="item"><t-button shape="circle" variant="outline">i</t-button></div>
+      <div class="item"><t-button shape="circle" variant="outline"><t-icon-calendar slot="icon"/></t-button></div>
     </div>
     <div class="tdesign-demo-block">
       <div class="item"><t-button shape="square" variant="dashed">虚框按钮</t-button></div>
       <div class="item"><t-button shape="round" variant="dashed">虚框按钮</t-button></div>
-      <div class="item"><t-button shape="circle" variant="dashed">i</t-button></div>
+      <div class="item"><t-button shape="circle" variant="dashed"><t-icon-calendar slot="icon"/></t-button></div>
     </div>
     <div class="tdesign-demo-block">
       <div class="item"><t-button shape="square" variant="text">文字按钮</t-button></div>
       <div class="item"><t-button shape="round" variant="text">文字按钮</t-button></div>
-      <div class="item"><t-button shape="circle" variant="text">i</t-button></div>
+      <div class="item"><t-button shape="circle" variant="text"><t-icon-calendar slot="icon"/></t-button></div>
     </div>
   </div>
 </template>
+<script>
+import TIconCalendar from '@tencent/tdesign-vue/lib/icon/calendar';
 
+export default {
+  components: {
+    TIconCalendar,
+  },
+};
+</script>
 <style lang="less" scoped>
 .tdesign-demo-item__body {
   .t-button + .t-button {
