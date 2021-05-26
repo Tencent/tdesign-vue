d55d309484c1cc647e5903d4e2e9b41aec33ce9c
chrysalis1215 <785851812@qq.com>
2021-05-26 18:08:07 +0800

    feat(breadcrumb): 面包屑超长部分使用tooltip展示(#327) (merge request !455)
    Squash merge branch 'feature/breadcrumb' into 'develop'
    
    * refactor(util): 添加doml类别工具方法判断节点是否溢出
    
    * feat(breadcrumb): 面包屑超长部分使用tooltip展示(#327)

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -218,3 +218,15 @@ export const clickOut = (els: Vue | Element | Iterable<any> | ArrayLike<any>, cb
     }
   });
 };
+
+// 用于判断节点内容是否溢出
+export const isNodeOverflow = (ele: Vue | Element | Vue[] | Element[]): boolean => {
+  const { clientWidth = 0, scrollWidth = 0 } = (
+    ele as Element & { clientWidth: number; scrollWidth: number }
+  );
+
+  if (scrollWidth > clientWidth) {
+    return true;
+  }
+  return false;
+};
