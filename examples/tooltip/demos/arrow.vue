7b5e262982fe24c3b73385ea932d06516ae2ed38
chrysalis1215 <785851812@qq.com>
2021-07-07 20:35:11 +0800

    hotfix/20210705_style (merge request !540)
    Squash merge branch 'hotfix/20210705_style' into 'develop'
    
    * feat:  list demo 样式优化
    
    * feat(list):  list demo 修改
    
    * feat(popup): demo 修改
    
    * chore(tooltips): tooltips demo 修改

--- examples/tooltip/demos/arrow.vue
+++ examples/tooltip/demos/arrow.vue
@@ -1,5 +1,7 @@
 <template>
-  <t-tooltip content="文字提示仅展示文本内容">
-    <t-button variant="outline">默认文字提示</t-button>
-  </t-tooltip>
+  <div>
+    <t-tooltip content="文字提示仅展示文本内容" trigger="click">
+      <t-button variant="outline">默认文字提示</t-button>
+    </t-tooltip>
+  </div>
 </template>
