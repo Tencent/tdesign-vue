bc2534f832d2b126b8620e7aed8ae5c7854073e2
chaishi <974383157@qq.com>
2021-07-14 13:57:18 +0800

    Loading/Notification/Message 插件处理 (merge request !559)
    Squash merge branch 'fix/0714/plugin' into 'develop'
    
    * feat(notify): support both notify and notification
    
    * test(notification/message/loading): update snapshots
    
    * feat(notification): support some more function

--- examples/loading/demos/service.vue
+++ examples/loading/demos/service.vue
@@ -1,37 +1,65 @@
 <template>
   <div>
-    <div id="loading-service-demo" class="loading-service-demo">我是service的容器</div>
-    <t-button @click="showFullScreen" size="small">服务方式加载（全屏）</t-button>
-    <t-button @click="showAttach" class="t-loading__btn" size="small" :disabled="attachLoading"
-    >服务方式加载（局部）</t-button
-    >
+    <div id="loading-service-demo" ref='content' class="loading-service-demo">我是service的容器</div>
+    <br>
+    <p>this.$loading(true)</p>
+    <p>this.$loading({ attach: '#loading-service-demo', showOverlay: true })</p>
+    <br>
+    <t-button @click="showFullScreen1" size="small">服务方式加载（全屏）</t-button>
+    <t-button @click="showAttach1" class="t-loading__btn" size="small" :disabled="attachLoading">服务方式加载（局部）</t-button>
+
+    <br><br>
+    <p>LoadingPlugin(true)</p>
+    <p>LoadingPlugin({ attach: '#loading-service-demo', showOverlay: true })</p>
+    <br>
+    <t-button @click="showFullScreen2" size="small">服务方式加载（全屏）</t-button>
+    <t-button @click="showAttach2" class="t-loading__btn" size="small" :disabled="attachLoading">服务方式加载（局部）</t-button>
   </div>
 </template>
 <script>
+import { LoadingPlugin } from '@tencent/tdesign-vue';
 export default {
   name: 'LoadingPlugin',
   data() {
     return {
-      loadingAttachInstance: null,
       attachLoading: false,
     };
   },
   methods: {
-    showFullScreen() {
+    showFullScreen1() {
       this.$loading(true);
       const timer = setTimeout(() => {
         this.$loading(false);
         clearTimeout(timer);
       }, 1000);
     },
-    showAttach() {
-      this.loadingAttachInstance = this.$loading({
+    showAttach1() {
+      const loadingAttachInstance = this.$loading({
         attach: '#loading-service-demo',
         showOverlay: true,
       });
       this.attachLoading = true;
       const timer = setTimeout(() => {
-        this.loadingAttachInstance.hide();
+        loadingAttachInstance.hide();
+        this.attachLoading = false;
+        clearTimeout(timer);
+      }, 1000);
+    },
+    showFullScreen2() {
+      LoadingPlugin(true);
+      const timer = setTimeout(() => {
+        LoadingPlugin(false);
+        clearTimeout(timer);
+      }, 1000);
+    },
+    showAttach2() {
+      const loadingAttachInstance = LoadingPlugin({
+        attach: () => this.$refs.content,
+        showOverlay: true,
+      });
+      this.attachLoading = true;
+      const timer = setTimeout(() => {
+        loadingAttachInstance.hide();
         this.attachLoading = false;
         clearTimeout(timer);
       }, 1000);
--- examples/message/demos/plugin.vue
+++ examples/message/demos/plugin.vue
@@ -1,62 +1,33 @@
 <template>
   <div>
-    <p>组件内调用-信息设置：this.$message.info('用户表示普通操作信息提示')</p>
-    <br>
-    <div class='t-demo-message-theme'>
-      <t-button @click="$message.info('用户表示普通操作信息提示')">info</t-button>
-      <t-button @click="$message.success('用户表示操作顺利达成')">success</t-button>
-      <t-button @click="$message.warning('用户表示操作引起一定后果')">warning</t-button>
-      <t-button @click="$message.error('用户表示操作引起严重的后果')">error</t-button>
-      <t-button @click="$message.question('用于帮助用户操作的信息提示')">question</t-button>
-      <t-button @click="$message.loading('用于表示操作正在生效的过程中')">loading</t-button>
-    </div>
-    <br><br>
-
-    <p>组件内调用-时间设置：this.$message.info('用户表示普通操作信息提示', 1000)</p>
+    <p>组件实例方法调用</p>
+    <p>this.$message.success('用户表示普通操作信息提示')</p>
+    <p>this.$message.success({ content: '用户表示普通操作信息提示', duration: 1000 })</p>
+    <p>this.$message('success', '用户表示普通操作信息提示')</p>
+    <p>this.$message('success', { content: '用户表示普通操作信息提示', duration: 1000 })</p>
     <br>
     <div class='t-demo-message-duration'>
       <t-button @click="$message.info('用户表示普通操作信息提示', 1000)">info</t-button>
-      <t-button @click="$message.success('用户表示操作顺利达成', 2000)">success</t-button>
-      <t-button @click="$message.warning('用户表示操作引起一定后果')">warning</t-button>
-      <t-button @click="$message.error('用户表示操作引起严重的后果', 4000)">error</t-button>
+      <t-button @click="$message.success({ content: '用户表示操作顺利达成', duration: 2000 })">success</t-button>
+      <t-button @click="$message('warning', '用户表示操作引起一定后果')">warning</t-button>
+      <t-button @click="$message('error', { content: '用户表示操作引起严重的后果', duration: 4000 })">error</t-button>
       <t-button @click="$message.question('用于帮助用户操作的信息提示', 5000)">question</t-button>
       <!-- 0 表示永远不自动消失 -->
       <t-button @click="$message.loading('用于表示操作正在生效的过程中', 0)">loading</t-button>
     </div>
     <br><br>
 
-
-    <p>组件内调用-设置更多内容：this.$message.info({ content: '用户表示普通操作信息提示', duration: 1000 })</p>
-    <br>
-    <div class='t-demo-message-obj'>
-      <t-button @click="$message.info({ content: '用户表示普通操作信息提示' })">普通信息</t-button>
-      <t-button @click="$message.success({ content: '用户表示操作顺利达成', closeBtn: true })">显示关闭按钮</t-button>
-      <t-button @click="$message.warning({ content: '用户表示操作引起一定后果', duration: 5000 })">设置显示时间</t-button>
-      <t-button @click="$message.error({ content: '用户表示操作引起严重的后果', content })">自定义内容</t-button>
-      <t-button @click="$message.question({ content: '用于帮助用户操作的信息提示', closeBtn })">自定义关闭按钮</t-button>
-      <t-button @click="$message.loading({ content: '用于表示操作正在生效的过程中', duration: 5000 })">loading</t-button>
-    </div>
-    <br><br>
-
-    <p>组件内调用-主函数：this.$message({ content: '用户表示普通操作信息提示', duration: 1000 })</p>
-    <br>
-    <div class='t-demo-message-main'>
-      <t-button @click="$message('info', { content: '用户表示普通操作信息提示' })">普通信息</t-button>
-      <t-button @click="$message('success',{ content: '用户表示操作顺利达成', closeBtn: true })">显示关闭按钮</t-button>
-      <t-button @click="$message('warning',{ content: '用户表示操作引起一定后果' }, 5000)">设置显示时间</t-button>
-      <t-button @click="$message('error', { content: '用户表示操作引起严重的后果' })">自定义内容</t-button>
-      <t-button @click="$message('question', { content: '用于帮助用户操作的信息提示', closeBtn })">自定义关闭按钮</t-button>
-      <t-button @click="$message('loading',{ content: '用于表示操作正在生效的过程中' }, 1000)">loading</t-button>
-    </div>
-    <br><br>
-
-    <p>按需引入：MessagePlugin.info({ content: '用户表示普通操作信息提示', duration: 1000 })</p>
+    <p>插件方法调用</p>
+    <p>MessagePlugin.success('用户表示普通操作信息提示')</p>
+    <p>MessagePlugin.success({ content: '用户表示普通操作信息提示', duration: 1000 })</p>
+    <p>MessagePlugin('success', '用户表示普通操作信息提示')</p>
+    <p>MessagePlugin('success', { content: '用户表示普通操作信息提示', duration: 1000 })</p>
     <br>
     <div class='t-demo-message-theme'>
       <t-button @click="MessagePlugin.info('用户表示普通操作信息提示')">info</t-button>
-      <t-button @click="MessagePlugin.success('用户表示操作顺利达成')">success</t-button>
-      <t-button @click="MessagePlugin.warning('用户表示操作引起一定后果')">warning</t-button>
-      <t-button @click="MessagePlugin.error('用户表示操作引起严重的后果')">error</t-button>
+      <t-button @click="MessagePlugin('success', '用户表示操作顺利达成')">success</t-button>
+      <t-button @click="MessagePlugin('warning', { content: '用户表示操作引起一定后果' })">warning</t-button>
+      <t-button @click="MessagePlugin.error({ content: '用户表示操作引起严重的后果' })">error</t-button>
       <t-button @click="MessagePlugin.question('用于帮助用户操作的信息提示')">question</t-button>
       <t-button @click="MessagePlugin.loading('用于表示操作正在生效的过程中')">loading</t-button>
     </div>
