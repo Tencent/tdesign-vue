aeed663086fc0f8cd1b2c683d342cb7f9c489c42
HQ-Lin <a154571896@gmail.com>
2021-08-19 16:16:56 +0800

    fix/demos/ui-error (merge request !690)
    Squash merge branch 'fix/demos/ui-error' into 'develop'
    
    * fix: 修复 组件 ui 细节
    
    * fix: 修复 组件 ui 细节
    
    * fix: 修复 组件 ui 细节
    
    * fix: 修复 组件 ui 细节

--- examples/layout/demos/combine.vue
+++ examples/layout/demos/combine.vue
@@ -8,11 +8,10 @@
           <t-menu-item value="item2">菜单内容一</t-menu-item>
           <t-menu-item value="item3">菜单内容二</t-menu-item>
           <t-menu-item value="item4" :disabled="true">菜单内容三</t-menu-item>
-          <div slot="options">
-            <a href="javascript:;"><t-icon name="search"/></a>
-            <a href="javascript:;"><t-icon name="notification-filled"/></a>
-            <a href="javascript:;"><t-icon name="home"/></a>
-          </div>
+          <template slot="options">
+            <span><t-icon name="search"/></span>
+            <span><t-icon name="home"/></span>
+          </template>
         </t-head-menu>
       </t-header>
       <t-layout>
