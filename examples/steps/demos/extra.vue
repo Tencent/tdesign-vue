02ce4d737a84db532ede5d9737af76091a8d38ee
uyarn <uyarnchen@gmail.com>
2021-09-02 20:29:55 +0800

    fix(step): 处理step demo错误 (merge request !725)
    Squash merge branch 'fix/steps/0902' into 'develop'
    
    * fix(step): 处理step demo错误

--- examples/steps/demos/extra.vue
+++ examples/steps/demos/extra.vue
@@ -13,6 +13,7 @@
     </t-step-item>
     <t-step-item title="步骤3" content="这里是提示文字">
       <template slot="extra" v-if="current === 2">
+        <t-button size="small" variant="text" @click.stop="current--">Pre</t-button>
         <t-button size="small" variant="base" @click.stop="current++">Next</t-button>
       </template>
     </t-step-item>
