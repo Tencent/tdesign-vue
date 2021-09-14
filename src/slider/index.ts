43fc6a74381a0792dee4ed19957861124129ce3a
xiaosansiji <799552341@qq.com>
2021-09-14 16:38:03 +0800

    fix: slider export error (merge request !754)
    Squash merge branch 'fix/slider-export' into 'develop'
    fix: slider export error
    - 修复 slider 导出问题，增加 type 定义导出
    - 顺带更新 common id

--- src/slider/index.ts
+++ src/slider/index.ts
@@ -1,6 +1,11 @@
-import Slider from './slider';
+import _Slider from './slider';
 import withInstall from '../utils/withInstall';
+import { TdSliderProps, Marks } from './type';
 
-withInstall('Slider', Slider);
-export { Slider };
+import './style';
+
+export const Slider = withInstall('Slider', _Slider);
+export type SliderProps = TdSliderProps;
+export type SliderMarks = Marks;
+export { SliderValue } from './type';
 export default Slider;
