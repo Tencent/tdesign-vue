6f3344b43f9beb3a7acd235a9a1cc4a171b47fef
pengYYYYY <pengyue970715@gmail.com>
2021-11-02 17:39:13 +0800

    feat/select/placeholder (merge request !846)
    Squash merge branch 'feat/select/placeholder' into 'develop'
    修复select无placeholder
    
    修复前
    
    <img width="" src="/uploads/03F006D9411B46649B0B4BCBE0FC79F9/image.png" alt="image.png" />
    
    修复后
    
    <img width="" src="/uploads/4444E68CEABA4C2DBDFEC9D53641B438/image.png" alt="image.png" />
    
    demo不变

--- src/select/select.tsx
+++ src/select/select.tsx
@@ -431,6 +431,9 @@ export default mixins(getLocalReceiverMixins('select')).extend({
       const useLocale = !this.loadingText && !this.$scopedSlots.loadingText;
       return useLocale ? this.t(this.locale.loadingText) : renderTNodeJSX(this, 'loadingText');
     },
+    getPlaceholderText() {
+      return this.placeholder || this.t(this.locale.placeholderText);
+    },
     getCloseIcon() {
       const closeIconClass = [`${name}-right-icon`, `${name}-right-icon__clear`];
       if (isFunction(this.locale.clearIcon)) {
@@ -462,7 +465,6 @@ export default mixins(getLocalReceiverMixins('select')).extend({
       popClass,
       size,
       showPlaceholder,
-      placeholder,
       selectedMultiple,
       multiple,
       showFilter,
@@ -482,6 +484,7 @@ export default mixins(getLocalReceiverMixins('select')).extend({
     const prefixIconSlot = renderTNodeJSX(this, 'prefixIcon');
     const emptySlot = this.getEmpty();
     const loadingTextSlot = this.getLoadingText();
+    const placeholderText = this.getPlaceholderText();
     return (
       <div ref='select' class={`${name}-wrap`}>
         <Popup
@@ -506,7 +509,7 @@ export default mixins(getLocalReceiverMixins('select')).extend({
             }
             {
               showPlaceholder && (
-                <span class={`${name}-placeholder`}> { placeholder }</span>
+                <span class={`${name}-placeholder`}> { placeholderText }</span>
               )
             }
             {this.valueDisplay || this.$scopedSlots.valueDisplay
