ef50d3a70c8bfb3f6067a5bc69c6330091f69b6d
HQ-Lin <a154571896@gmail.com>
2021-11-02 13:05:50 +0800

    feat: 优化 radio demo (merge request !845)
    Squash merge branch 'fix/radio/demo' into 'develop'
    
    * feat: 优化 radio demo

--- examples/radio/demos/group.vue
+++ examples/radio/demos/group.vue
@@ -10,23 +10,6 @@
     </div>
 
     <div>
-      <t-radio-group
-        default-value="3"
-        :options="options"
-        @change="onChange"
-      ></t-radio-group>
-    </div>
-
-    <div>
-      <t-radio-group
-        disabled
-        default-value="1"
-        :options="optionsWithDisabled"
-        @change="onChange"
-      ></t-radio-group>
-    </div>
-
-    <div>
       <t-radio-group default-value="1" @change="onChange">
         <t-radio value="1">选项一</t-radio>
         <t-radio value="2">选项二</t-radio>
@@ -58,26 +41,6 @@ export default {
         {
           value: '4',
           label: () => '选项四',
-        },
-      ],
-      optionsWithDisabled: [
-        {
-          value: '1',
-          label: '选项一',
-        },
-        {
-          value: '2',
-          label: '选项二',
-        },
-        {
-          value: '3',
-          label: '选项三',
-          disabled: false,
-        },
-        {
-          value: '4',
-          label: () => '选项四',
-          disabled: false,
         },
       ],
     };
--- examples/radio/demos/type.vue
+++ examples/radio/demos/type.vue
@@ -6,8 +6,16 @@
         <t-radio-button value="1">选项一</t-radio-button>
         <t-radio-button value="2">选项二</t-radio-button>
         <t-radio-button value="3">选项三</t-radio-button>
-        <t-radio-button value="1" disabled>选中禁用态</t-radio-button>
-        <t-radio-button value="2" disabled>未选中禁用态</t-radio-button>
+        <t-radio-button value="4">选项四</t-radio-button>
+        <t-radio-button value="5">选项五</t-radio-button>
+      </t-radio-group>
+
+      <t-radio-group disabled default-value="4">
+        <t-radio-button value="1">选项一</t-radio-button>
+        <t-radio-button value="2">选项二</t-radio-button>
+        <t-radio-button value="3">选项三</t-radio-button>
+        <t-radio-button value="4">选中禁用态</t-radio-button>
+        <t-radio-button>未选中禁用态</t-radio-button>
       </t-radio-group>
     </div>
     <div class="tdesign-demo-block-column">
