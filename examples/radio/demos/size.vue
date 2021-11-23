9aa36c1201bee3398345427ebebd2dc7d9d47ee9
HQ-Lin <a154571896@gmail.com>
2021-11-23 14:19:30 +0800

    feat: 优化demo 样式 (merge request !924)
    Squash merge branch 'fix/menu/demo' into 'develop'
    
    * feat: 优化demo 样式
    * feat: 优化demo 样式

--- examples/radio/demos/size.vue
+++ examples/radio/demos/size.vue
@@ -1,60 +1,24 @@
 <template>
-  <div class="tdesign-demo-block-row">
-    <div class="tdesign-demo-block-column">
-      <div>
-        <t-radio-group size="small" default-value="1">
-          <t-radio-button value="1">选项一</t-radio-button>
-          <t-radio-button value="2">选项二</t-radio-button>
-          <t-radio-button value="3">选项三</t-radio-button>
-          <t-radio-button value="4">选项四</t-radio-button>
-        </t-radio-group>
-      </div>
+  <div class="tdesign-demo-block-column">
+    <t-radio-group size="small" default-value="1">
+      <t-radio-button value="1">选项一</t-radio-button>
+      <t-radio-button value="2">选项二</t-radio-button>
+      <t-radio-button value="3">选项三</t-radio-button>
+      <t-radio-button value="4">选项四</t-radio-button>
+    </t-radio-group>
 
-      <div>
-        <t-radio-group default-value="2">
-          <t-radio-button value="1">选项一</t-radio-button>
-          <t-radio-button value="2">选项二</t-radio-button>
-          <t-radio-button value="3">选项三</t-radio-button>
-          <t-radio-button value="4">选项四</t-radio-button>
-        </t-radio-group>
-      </div>
+    <t-radio-group default-value="2">
+      <t-radio-button value="1">选项一</t-radio-button>
+      <t-radio-button value="2">选项二</t-radio-button>
+      <t-radio-button value="3">选项三</t-radio-button>
+      <t-radio-button value="4">选项四</t-radio-button>
+    </t-radio-group>
 
-      <div>
-        <t-radio-group size="large" default-value="3">
-          <t-radio-button value="1">选项一</t-radio-button>
-          <t-radio-button value="2">选项二</t-radio-button>
-          <t-radio-button value="3">选项三</t-radio-button>
-          <t-radio-button value="4">选项四</t-radio-button>
-        </t-radio-group>
-      </div>
-    </div>
-    <div class="tdesign-demo-block-column">
-      <div>
-        <t-radio-group size="small" variant="default-filled" default-value="1">
-          <t-radio-button value="1">选项一</t-radio-button>
-          <t-radio-button value="2">选项二</t-radio-button>
-          <t-radio-button value="3">选项三</t-radio-button>
-          <t-radio-button value="4">选项四</t-radio-button>
-        </t-radio-group>
-      </div>
-
-      <div>
-        <t-radio-group variant="default-filled" default-value="2">
-          <t-radio-button value="1">选项一</t-radio-button>
-          <t-radio-button value="2">选项二</t-radio-button>
-          <t-radio-button value="3">选项三</t-radio-button>
-          <t-radio-button value="4">选项四</t-radio-button>
-        </t-radio-group>
-      </div>
-
-      <div>
-        <t-radio-group size="large" variant="default-filled" default-value="3">
-          <t-radio-button value="1">选项一</t-radio-button>
-          <t-radio-button value="2">选项二</t-radio-button>
-          <t-radio-button value="3">选项三</t-radio-button>
-          <t-radio-button value="4">选项四</t-radio-button>
-        </t-radio-group>
-      </div>
-    </div>
+    <t-radio-group size="large" default-value="3">
+      <t-radio-button value="1">选项一</t-radio-button>
+      <t-radio-button value="2">选项二</t-radio-button>
+      <t-radio-button value="3">选项三</t-radio-button>
+      <t-radio-button value="4">选项四</t-radio-button>
+    </t-radio-group>
   </div>
 </template>
