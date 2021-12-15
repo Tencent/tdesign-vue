a561f19d33924d4f739981e269e42d448a0e1e7b
HQ-Lin <a154571896@gmail.com>
2021-12-15 16:18:39 +0800

    feat: 优化开发细节

--- site/src/components/codesandbox/index.vue
+++ site/src/components/codesandbox/index.vue
@@ -20,12 +20,13 @@
 </template>
 
 <script>
+import pkg from '@/package.json';
 import packageJSON from './package.json';
 import mainJsContent from './main.js?raw';
 import styleContent from './index.css?raw';
 
 packageJSON.dependencies['tdesign-vue'] = __VERSION__; // eslint-disable-line
-packageJSON.dependencies['tdesign-icons-vue'] = '0.0.6';
+packageJSON.dependencies['tdesign-icons-vue'] = pkg.dependencies['tdesign-icons-vue'];
 
 const packageJSONContent = JSON.stringify(packageJSON, null, 2);
 
