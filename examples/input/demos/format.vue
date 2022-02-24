d68686362283eeff235da99e135ce555f5df8605
mokywu <1209422267@qq.com>
2022-02-24 10:44:38 +0800

    feat(input): 增加format属性
    增加format属性

---
+++ examples/input/demos/format.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="tdesign-demo-block-column" style="max-width: 500px">
+    <t-input v-model="input" :status="inputStatus" :format="format" placeholder="请输入数字" :tips="tips" />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      input: '',
+    };
+  },
+  computed: {
+    inputStatus() {
+      if (isNaN(+this.input)) {
+        return 'error';
+      }
+      return '';
+    },
+    tips() {
+      if (!this.inputStatus) {
+        return '';
+      }
+      return '请输入数字';
+    },
+  },
+  methods: {
+    format(val) {
+      const reg = /(\d)(?=(?:\d{3})+$)/g;
+      const str = val.replace(reg, '$1,');
+      return str;
+    },
+  },
+};
+</script>
