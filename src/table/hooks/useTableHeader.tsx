c21ddfe017b55810f7d4f6a162e8b6952e638500
chaishi <yaoyanhuoyi@qq.com>
2022-03-26 19:12:13 +0800

    refactor(table): typo error

--- src/table/hooks/useTableHeader.tsx
+++ src/table/hooks/useTableHeader.tsx
@@ -3,12 +3,12 @@ import isString from 'lodash/isString';
 import isFunction from 'lodash/isFunction';
 import { CreateElement } from 'vue';
 import { TdBaseTableProps } from '../type';
-import { TableColums, getThRowspanAndColspan, getThList } from './useMultiHeader';
+import { TableColumns, getThRowspanAndColspan, getThList } from './useMultiHeader';
 import useClassName from './useClassName';
 import { TNodeReturnValue } from '../../common';
 
 // 渲染表头的通用方法
-export function renderTitle(h: CreateElement, slots: SetupContext['slots'], col: TableColums[0], index: number) {
+export function renderTitle(h: CreateElement, slots: SetupContext['slots'], col: TableColumns[0], index: number) {
   const params = { col, colIndex: index };
   if (isFunction(col.title)) {
     return col.title(h, params);
