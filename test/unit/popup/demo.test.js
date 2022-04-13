79a27be82bd03ad2f333af47d89ec347185201c9
azhe <xiaosansiji@gmail.com>
2022-04-13 21:39:16 +0800

    Feat/generate demo test (#735)
    * feat: generate test automatically
    
    * test: add demo test & update snapshot
    
    * feat: move generate script to common
    
    * fix: eslint error
    
    * test: update snapshot
    
    * docs: add test coverage to readme.doc
    
    * feat: update common

--- test/unit/popup/demo.test.js
+++ test/unit/popup/demo.test.js
@@ -1,29 +1,33 @@
+/**
+ * 该文件为由脚本 `npm run test:demo` 自动生成，如需修改，执行脚本命令即可。请勿手写直接修改，否则会被覆盖
+ */
+
 import { mount } from '@vue/test-utils';
-import base from '@/examples/popup/demos/base.vue';
-import container from '@/examples/popup/demos/container.vue';
-import destroy from '@/examples/popup/demos/destroy.vue';
-import disabled from '@/examples/popup/demos/disabled.vue';
-import placement from '@/examples/popup/demos/placement.vue';
-import style from '@/examples/popup/demos/style.vue';
-import triggerElement from '@/examples/popup/demos/trigger-element.vue';
-import trigger from '@/examples/popup/demos/trigger.vue';
-import visible from '@/examples/popup/demos/visible.vue';
+import baseVue from '@/examples/popup/demos/base.vue';
+import containerVue from '@/examples/popup/demos/container.vue';
+import destroyVue from '@/examples/popup/demos/destroy.vue';
+import disabledVue from '@/examples/popup/demos/disabled.vue';
+import placementVue from '@/examples/popup/demos/placement.vue';
+import styleVue from '@/examples/popup/demos/style.vue';
+import triggerElementVue from '@/examples/popup/demos/trigger-element.vue';
+import triggerVue from '@/examples/popup/demos/trigger.vue';
+import visibleVue from '@/examples/popup/demos/visible.vue';
 
 const mapper = {
-  base,
-  container,
-  destroy,
-  disabled,
-  placement,
-  style,
-  triggerElement,
-  trigger,
-  visible,
+  baseVue,
+  containerVue,
+  destroyVue,
+  disabledVue,
+  placementVue,
+  styleVue,
+  triggerElementVue,
+  triggerVue,
+  visibleVue,
 };
 
-describe('Progress', () => {
+describe('Popup', () => {
   Object.keys(mapper).forEach((demoName) => {
-    it(`${demoName} demo works fine`, () => {
+    it(`Popup ${demoName} demo works fine`, () => {
       const wrapper = mount(mapper[demoName]);
       expect(wrapper.element).toMatchSnapshot();
     });
