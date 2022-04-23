f9c8218dc8507b8673940a2b868b43b8847b34a7
pengYYY <pengyue970715@gmail.com>
2022-04-23 21:10:59 +0800

    fix: dialog alert error

--- src/dialog/props.ts
+++ src/dialog/props.ts
@@ -84,7 +84,7 @@ export default {
     type: Boolean,
     default: true,
   },
-  /** 仅在挂载元素中显示抽屉，默认在浏览器可视区域显示。父元素需要有定位属性，如：position: relative */
+  /** 【开发中】仅在挂载元素中显示抽屉，默认在浏览器可视区域显示。父元素需要有定位属性，如：position: relative */
   showInAttachedElement: Boolean,
   /** 是否显示遮罩层 */
   showOverlay: {
