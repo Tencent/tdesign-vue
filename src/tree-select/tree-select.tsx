da483e173890c0333a6832615d3f0b7529dad7c6
LoopZhou <363415642@qq.com>
2022-05-17 15:42:31 +0800

    fix(tree-select): 支持treeProps.keys.children字段配置

--- src/tree-select/tree-select.tsx
+++ src/tree-select/tree-select.tsx
@@ -214,6 +214,13 @@ export default mixins(getConfigReceiverMixins<Vue, TreeSelectConfig>('treeSelect
       }
       return 'value';
     },
+    realChildren(): string {
+      const { treeProps } = this;
+      if (!isEmpty(treeProps) && !isEmpty(treeProps.keys)) {
+        return treeProps.keys.children || 'children';
+      }
+      return 'children';
+    },
     tagList(): Array<TreeSelectValue> {
       if (this.nodeInfo && isArray(this.nodeInfo)) {
         return this.nodeInfo.map((node) => node.label);
@@ -381,7 +388,7 @@ export default mixins(getConfigReceiverMixins<Vue, TreeSelectConfig>('treeSelect
         if (data[i][this.realValue] === targetValue) {
           return { label: data[i][this.realLabel], value: data[i][this.realValue] };
         }
-        const childrenData = data[i]?.children;
+        const childrenData = data[i][this.realChildren];
         if (childrenData) {
           const data = Array.isArray(childrenData) ? childrenData : this.getTreeData();
           const result = this.getTreeNode(data, targetValue);
