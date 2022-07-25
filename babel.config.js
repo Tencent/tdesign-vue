126df2d8b36ab6edb28496f17c91ef98884e8c5a
brianfzhang <brian.netmad@gmail.com>
2022-07-25 16:41:40 +0800

    feat: 更改babel.config打包配置

--- babel.config.js
+++ babel.config.js
@@ -34,18 +34,18 @@ module.exports = {
       ],
     },
     production: {
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            modules: false,
-          },
-        ],
-        '@vue/babel-preset-jsx',
-      ],
+      presets: [['@babel/preset-env']],
       plugins: [
-        '@babel/plugin-transform-runtime',
+        ['@vue/babel-plugin-jsx', { mergeProps: false, enableObjectSlots: false }],
+        '@babel/plugin-proposal-optional-chaining',
+        '@babel/plugin-transform-object-assign',
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-proposal-export-default-from',
+        '@babel/plugin-proposal-export-namespace-from',
         '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-transform-runtime',
+        'transform-require-context',
       ],
     },
   },
