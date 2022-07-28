d46448012473ff2ccc971d7dd56f97a4fa79ac8b
chaishi <yaoyanhuoyi@qq.com>
2022-07-28 19:44:33 +0800

    feat(input-number): input-number supports slots

--- examples/input-number/demos/normal.vue
+++ examples/input-number/demos/normal.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-    <t-input-number
-      v-model="value"
-      theme="normal"
-      :max="15"
-      :min="-2"
-    ></t-input-number>
+    <div>
+      <t-input-number v-model="value" theme="normal" :max="15" :min="-2"></t-input-number>
+    </div>
+
+    <div>
+      <t-input-number v-model="value" theme="normal" align="right">
+        <template #label><span>金额：</span></template>
+        <template #suffix><span>元</span></template>
+      </t-input-number>
+    </div>
   </div>
 </template>
 
