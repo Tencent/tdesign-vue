a49b723a7cbbc2bb0f51f0d0fedcfc20f1613d5f
pengYYY <pengyue970715@gmail.com>
2022-07-31 21:03:58 +0800

    fix: cascader value check

--- src/cascader/core/helper.ts
+++ src/cascader/core/helper.ts
@@ -14,19 +14,16 @@ import {
  * @param cascaderContext
  * @returns
  */
-export function getSingleContent(cascaderContext: CascaderContextType) {
+export function getSingleContent(cascaderContext: CascaderContextType): string {
   const {
-    value, multiple, treeStore, showAllLevels, setValue,
+    value, multiple, treeStore, showAllLevels,
   } = cascaderContext;
   if (multiple || !value) return '';
 
   if (Array.isArray(value)) return '';
   const node = treeStore && treeStore.getNodes(value as TreeNodeValue | TreeNode);
   if (!(node && node.length)) {
-    if (value) {
-      setValue(multiple ? [] : '', 'invalid-value');
-    }
-    return '';
+    return value as string;
   }
   const path = node && node[0].getPath();
   if (path && path.length) {
