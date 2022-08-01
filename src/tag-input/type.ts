c54cc6f817dfa34912ba88df43c7f03e5007b40f
Ardor <44194929+ardor-zhang@users.noreply.github.com>
2022-08-01 15:55:54 +0800

    Feature/taginput tag draggable (#1227)
    * feat(tag-input): taginput tag draggable
    
    * docs(tag-input): dragsort docs
    
    * refactor(tag-input): move hooks to floder

--- src/tag-input/type.ts
+++ src/tag-input/type.ts
@@ -130,7 +130,7 @@ export interface TdTagInputProps {
    */
   onClear?: (context: { e: MouseEvent }) => void;
   /**
-   * 【开发中】拖拽排序时触发
+   * 拖拽排序时触发
    */
   onDragSort?: (context: TagInputDragSortContext) => void;
   /**
@@ -196,3 +196,42 @@ export interface TagInputRemoveContext {
 }
 
 export type TagInputRemoveTrigger = 'tag-remove' | 'backspace';
+
+interface DragSortContext<T> {
+  currentIndex: number;
+  current: T;
+  targetIndex: number;
+  target: T;
+}
+
+export interface DragSortProps<T> {
+  sortOnDraggable: boolean;
+  onDragSort?: (context: DragSortContext<T>) => void;
+  onDragOverCheck?: {
+    x?: boolean;
+    targetClassNameRegExp?: RegExp;
+  };
+}
+
+type DragFnType = (e?: DragEvent, index?: number, record?: any) => void;
+interface DragSortInnerData {
+  dragging?: boolean;
+  onDragStart?: DragFnType;
+  onDragOver?: DragFnType;
+  onDrop?: DragFnType;
+  onDragEnd?: DragFnType;
+}
+
+export interface DragProps {
+  (index?: number, record?: any): {
+    draggable: boolean;
+    onDragstart?: DragFnType;
+    onDragover?: DragFnType;
+    onDrop?: DragFnType;
+    onDragend?: DragFnType;
+  };
+}
+
+export interface DragSortInnerProps extends DragSortInnerData {
+  getDragProps?: DragProps;
+}
