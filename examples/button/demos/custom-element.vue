ed1faf87cb0718fab986bbbce5f684e3d5bc496e
paopao <zhangpaopao0609@gmail.com>
2022-08-18 15:52:07 +0800

    Feature/button support href tag (#1313)
    * feat(button): support tag and href
    
    * docs(button): add docs for href and tag
    
    * docs(button): tag and href en-us docs
    
    * feat(button): change attrs transfer way
    
    * docs(button): custom-element tag href docs
    
    * test(button): update snap

---
+++ examples/button/demos/custom-element.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="tdesign-demo-block-row">
+    <t-button tag="div">div</t-button>
+    <t-button tag="a">a</t-button>
+    <t-button href="#">a:href</t-button>
+  </div>
+</template>
