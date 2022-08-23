d56345e493e0a3d1e3da85260b9a4d0a0952a2e7
Tomao <2694235519@qq.com>
2022-08-23 10:20:45 +0800

    chore: 更新button和快照

--- examples/button/demos/shape.vue
+++ examples/button/demos/shape.vue
@@ -1,44 +1,28 @@
 <template>
   <t-space direction="vertical">
     <t-space>
-      <div class="item"><t-button theme="primary" shape="rectangle" variant="base">填充按钮</t-button></div>
-      <div class="item">
-        <t-button theme="primary" shape="square" variant="base"><calendar-icon slot="icon" /></t-button>
-      </div>
-      <div class="item"><t-button theme="primary" shape="round" variant="base">填充按钮</t-button></div>
-      <div class="item">
-        <t-button theme="primary" shape="circle" variant="base"><calendar-icon slot="icon" /></t-button>
-      </div>
+      <t-button theme="primary" shape="rectangle" variant="base">填充按钮</t-button>
+      <t-button theme="primary" shape="square" variant="base"><calendar-icon slot="icon" /></t-button>
+      <t-button theme="primary" shape="round" variant="base">填充按钮</t-button>
+      <t-button theme="primary" shape="circle" variant="base"><calendar-icon slot="icon" /></t-button>
     </t-space>
     <t-space>
-      <div class="item"><t-button shape="rectangle" variant="outline">描边按钮</t-button></div>
-      <div class="item">
-        <t-button shape="square" variant="outline"><calendar-icon slot="icon" /></t-button>
-      </div>
-      <div class="item"><t-button shape="round" variant="outline">描边按钮</t-button></div>
-      <div class="item">
-        <t-button shape="circle" variant="outline"><calendar-icon slot="icon" /></t-button>
-      </div>
+      <t-button shape="rectangle" variant="outline">描边按钮</t-button>
+      <t-button shape="square" variant="outline"><calendar-icon slot="icon" /></t-button>
+      <t-button shape="round" variant="outline">描边按钮</t-button>
+      <t-button shape="circle" variant="outline"><calendar-icon slot="icon" /></t-button>
     </t-space>
     <t-space>
-      <div class="item"><t-button shape="rectangle" variant="dashed">虚框按钮</t-button></div>
-      <div class="item">
-        <t-button shape="square" variant="dashed"><calendar-icon slot="icon" /></t-button>
-      </div>
-      <div class="item"><t-button shape="round" variant="dashed">虚框按钮</t-button></div>
-      <div class="item">
-        <t-button shape="circle" variant="dashed"><calendar-icon slot="icon" /></t-button>
-      </div>
+      <t-button shape="rectangle" variant="dashed">虚框按钮</t-button>
+      <t-button shape="square" variant="dashed"><calendar-icon slot="icon" /></t-button>
+      <t-button shape="round" variant="dashed">虚框按钮</t-button>
+      <t-button shape="circle" variant="dashed"><calendar-icon slot="icon" /></t-button>
     </t-space>
     <t-space>
-      <div class="item"><t-button shape="rectangle" variant="text">文字按钮</t-button></div>
-      <div class="item">
-        <t-button shape="square" variant="text"><calendar-icon slot="icon" /></t-button>
-      </div>
-      <div class="item"><t-button shape="round" variant="text">文字按钮</t-button></div>
-      <div class="item">
-        <t-button shape="circle" variant="text"><calendar-icon slot="icon" /></t-button>
-      </div>
+      <t-button shape="rectangle" variant="text">文字按钮</t-button>
+      <t-button shape="square" variant="text"><calendar-icon slot="icon" /></t-button>
+      <t-button shape="round" variant="text">文字按钮</t-button>
+      <t-button shape="circle" variant="text"><calendar-icon slot="icon" /></t-button>
     </t-space>
   </t-space>
 </template>
