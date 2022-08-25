c28d09a3d9a7ad753b78c17db6c878571106860f
yangchao987 <73540904+yangchao987@users.noreply.github.com>
2022-08-25 16:19:54 +0800

    chore: update radio (#1375)
    Co-authored-by: yangchao <yangchao@xxpie.com>

--- examples/radio/demos/type.vue
+++ examples/radio/demos/type.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="tdesign-demo-block-column-large">
-    <div class="tdesign-demo-block-column">
+  <t-space direction="vertical" size="36px">
+    <t-space direction="vertical">
       <h5>边框型单选按钮</h5>
       <t-radio-group default-value="1">
         <t-radio-button value="1">选项一</t-radio-button>
@@ -17,10 +17,10 @@
         <t-radio-button value="4">选中禁用态</t-radio-button>
         <t-radio-button>未选中禁用态</t-radio-button>
       </t-radio-group>
-    </div>
-    <div class="tdesign-demo-block-column">
+    </t-space>
+    <t-space direction="vertical">
       <h5>填充型单选按钮</h5>
-      <div>
+      <t-space>
         <t-radio-group variant="default-filled" default-value="1">
           <t-radio-button value="1">选项一</t-radio-button>
           <t-radio-button value="2">选项二</t-radio-button>
@@ -28,8 +28,8 @@
           <t-radio-button value="4">选项四</t-radio-button>
           <t-radio-button value="5">选项五</t-radio-button>
         </t-radio-group>
-      </div>
-      <div>
+      </t-space>
+      <t-space>
         <t-radio-group variant="primary-filled" default-value="1">
           <t-radio-button value="1">选项一</t-radio-button>
           <t-radio-button value="2">选项二</t-radio-button>
@@ -37,8 +37,8 @@
           <t-radio-button value="4">选项四</t-radio-button>
           <t-radio-button value="5">选项五</t-radio-button>
         </t-radio-group>
-      </div>
-      <div>
+      </t-space>
+      <t-space>
         <t-radio-group variant="default-filled" default-value="4">
           <t-radio-button value="1" disabled>选项一</t-radio-button>
           <t-radio-button value="2" disabled>选项二</t-radio-button>
@@ -46,8 +46,8 @@
           <t-radio-button value="4" disabled>选中禁用态</t-radio-button>
           <t-radio-button value="5" disabled>未选中禁用态</t-radio-button>
         </t-radio-group>
-      </div>
-      <div>
+      </t-space>
+      <t-space>
         <t-radio-group variant="primary-filled" default-value="4">
           <t-radio-button value="1" disabled>选项一</t-radio-button>
           <t-radio-button value="2" disabled>选项二</t-radio-button>
@@ -55,7 +55,7 @@
           <t-radio-button value="4" disabled>选中禁用态</t-radio-button>
           <t-radio-button value="5" disabled>未选中禁用态</t-radio-button>
         </t-radio-group>
-      </div>
-    </div>
-  </div>
+      </t-space>
+    </t-space>
+  </t-space>
 </template>
