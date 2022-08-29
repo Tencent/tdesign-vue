3d8a04e5b92e44559f6eedfd266f5638ede9a879
skytt <66660723@qq.com>
2022-08-29 10:15:29 +0800

    fix(select): 去除组件注册时的 map props

--- src/select/index.ts
+++ src/select/index.ts
@@ -1,25 +1,20 @@
 import VueCompositionAPI from '@vue/composition-api';
+
 import _Select from './select';
 import _Option from './option';
 import _OptionGroup from './optionGroup';
-import mapProps from '../utils/map-props';
 import withInstall from '../utils/withInstall';
 import { TdSelectProps, TdOptionProps, TdOptionGroupProps } from './type';
 
 import './style';
 
+export * from './type';
 export type SelectProps = TdSelectProps;
 export type OptionProps = TdOptionProps;
 export type OptionGroupProps = TdOptionGroupProps;
-export * from './type';
 
-export const Select = withInstall(
-  mapProps(['value'], {
-    model: { prop: 'value', event: 'change' },
-  })(_Select),
-  VueCompositionAPI,
-);
-
+export const Select = withInstall(_Select, VueCompositionAPI);
 export const Option = withInstall(_Option, VueCompositionAPI);
-
 export const OptionGroup = withInstall(_OptionGroup, VueCompositionAPI);
+
+export default Select;
