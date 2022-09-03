6fe7efd3308dfe41bdebcb73a2cd9d0394ff2d11
chaishi <yaoyanhuoyi@qq.com>
2022-09-03 15:35:27 +0800

    test(table): update snapshots

--- src/alert/_example/operation.vue
+++ src/alert/_example/operation.vue
@@ -1,11 +1,11 @@
 <template>
-  <t-sapce>
+  <t-space>
     <t-alert theme="info" message="这是一条普通的消息提示" close>
       <template #operation>
         <span @click="handleOperation">相关操作</span>
       </template>
     </t-alert>
-  </t-sapce>
+  </t-space>
 </template>
 <script lang="jsx">
 export default {
