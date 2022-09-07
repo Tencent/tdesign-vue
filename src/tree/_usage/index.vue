a5c67ff33b748d05ac38e3c55c0d24abb26636d2
Kyrielin <a154571896@gmail.com>
2022-09-07 14:39:33 +0800

    chore: 修复 tree live demo 问题 (#1477)

--- src/tree/_usage/index.vue
+++ src/tree/_usage/index.vue
@@ -2,20 +2,7 @@
 <template>
   <base-usage :code="usageCode" :config-list="configList" :panel-list="panelList" @PanelChange="onPanelChange">
     <template #tree="{ configProps }">
-      <t-tree
-        :data="[
-          { label: '第一段', children: [{ label: '第二段' }, { label: '第二段' }] },
-          {
-            label: '第一段',
-            children: [{ label: '第二段' }, { label: '第二段' }],
-          },
-          {
-            label: '第一段',
-            children: [{ label: '第二段' }, { label: '第二段' }],
-          },
-        ]"
-        v-bind="configProps"
-      />
+      <t-tree :data="data" v-bind="configProps" />
     </template>
   </base-usage>
 </template>
@@ -24,13 +11,22 @@
 /* eslint-disable */
 import { ref, onMounted } from '@vue/composition-api';
 import configJson from './props.json';
+const data = ref([
+  { label: '第一段', children: [{ label: '第二段' }, { label: '第二段' }] },
+  {
+    label: '第一段',
+    children: [{ label: '第二段' }, { label: '第二段' }],
+  },
+  {
+    label: '第一段',
+    children: [{ label: '第二段' }, { label: '第二段' }],
+  },
+]);
 
 const configList = ref(configJson);
 const panelList = [{ label: 'tree', value: 'tree' }];
 
-const usageCodeMap = {
-  tree: "\n        <t-tree :data=\"[{ label: '第一段',\n          children: [ { label: '第二段' }, { label: '第二段' } ],\n        },{\n          label: '第一段',\n          children: [ { label: '第二段' }, { label: '第二段' } ],\n        },{\n          label: '第一段',\n          children: [ { label: '第二段' }, { label: '第二段' } ],\n        }]\" v-bind=\"configProps\" />\n      ",
-};
+const usageCodeMap = { tree: '\n        <t-tree :data="data" v-bind="configProps" />\n      ' };
 const usageCode = ref(`<template>${usageCodeMap[panelList[0].value].trim()}</template>`);
 
 function onPanelChange(panel) {
