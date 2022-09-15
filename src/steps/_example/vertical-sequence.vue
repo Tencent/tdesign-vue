4e9913b39641db9fd57dee8896842a58aa17d7e5
Micro-sun <1306714976@qq.com>
2022-09-15 15:04:21 +0800

    fix: 修复steps步骤条

--- src/steps/_example/vertical-sequence.vue
+++ src/steps/_example/vertical-sequence.vue
@@ -8,8 +8,8 @@
     </t-steps>
     <t-steps layout="vertical" sequence="reverse" :current="1" readonly>
       <t-step-item title="已完成的步骤" content="这里是提示文字"></t-step-item>
+      <t-step-item title="已完成的步骤" content="这里是提示文字"></t-step-item>
       <t-step-item title="进行中的步骤" content="这里是提示文字"></t-step-item>
-      <t-step-item title="未进行的步骤" content="这里是提示文字"></t-step-item>
       <t-step-item title="未进行的步骤" content="这里是提示文字"></t-step-item>
     </t-steps>
   </t-space>
