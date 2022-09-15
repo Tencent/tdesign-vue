d7c6b39fe3f2bff47f2cfd1f5492c9075019b89c
pengYYY <pengyue970715@gmail.com>
2022-09-15 15:07:04 +0800

    fix: select-input demo style

--- src/select-input/_example/autowidth-multiple.vue
+++ src/select-input/_example/autowidth-multiple.vue
@@ -104,7 +104,6 @@ export default {
 .tdesign-demo__panel-options-borderless-multiple .t-checkbox {
   display: flex;
   border-radius: 3px;
-  height: 40px;
   line-height: 22px;
   cursor: pointer;
   padding: 9px 8px;
