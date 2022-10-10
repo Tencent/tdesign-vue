1cbfcb3dca2680ede5e5645c6660a67eb58a0c29
Uyarn <uyarnchen@gmail.com>
2022-10-10 18:29:32 +0800

    chore: update snapshot

--- src/dropdown/hooks/useDropdownOptions.ts
+++ src/dropdown/hooks/useDropdownOptions.ts
@@ -5,10 +5,10 @@ import { DropdownOption, TdDropdownProps } from '../type';
 const DropdownMenuName = 't-dropdown-menu';
 
 export const getOptionsFromChildren = (menuGroup: any): DropdownOption[] => {
-  if (!menuGroup || menuGroup.length === 0) return [];
+  if (!menuGroup || menuGroup?.length === 0) return [];
 
   // 处理内部嵌套场景
-  if (menuGroup.tag === DropdownMenuName) {
+  if (menuGroup?.tag === DropdownMenuName) {
     const groupChildren = menuGroup.children;
     if (Array.isArray(groupChildren)) {
       return getOptionsFromChildren(groupChildren);
@@ -18,21 +18,25 @@ export const getOptionsFromChildren = (menuGroup: any): DropdownOption[] => {
 
   if (Array.isArray(menuGroup)) {
     return menuGroup.map((item) => {
-      const groupChildren = item.componentOptions.children;
-      const contentIdx = groupChildren.findIndex((v: VNode) => typeof v.text === 'string');
-      const childrenContent = groupChildren.filter((v: VNode) => typeof v.text !== 'string');
+      const groupChildren = item?.componentOptions?.children;
+      const contentIdx = groupChildren.findIndex?.((v: VNode) => typeof v.text === 'string');
+      const childrenContent = groupChildren.filter?.((v: VNode) => typeof v.text !== 'string');
+
+      const commonProps = {
+        ...item.componentOptions?.propsData,
+        style: item.componentOptions?.data?.style,
+        class: item.componentOptions?.data?.staticClass,
+        onClick: item.componentOptions?.listeners?.click,
+        content: groupChildren[contentIdx]?.text,
+      };
       if (childrenContent.length === 1) {
         return {
-          ...item.componentOptions?.propsData,
-          onClick: item.componentOptions?.listeners?.click,
-          content: groupChildren[contentIdx]?.text,
+          ...commonProps,
           children: childrenContent.length > 0 ? getOptionsFromChildren(childrenContent[0].componentOptions) : null,
         };
       }
       return {
-        ...item.componentOptions?.propsData,
-        onClick: item.componentOptions?.listeners?.click,
-        content: groupChildren[contentIdx]?.text,
+        ...commonProps,
         children: childrenContent.length > 0 ? getOptionsFromChildren(childrenContent) : null,
       };
     });
@@ -47,7 +51,7 @@ export default function useDropdownOptions(
     [key: string]: VNode[];
   },
 ): ComputedRef<DropdownOption[]> {
-  const menuSlot = slots?.default?.filter((v: VNode) => v.componentOptions.tag === DropdownMenuName)?.[0]?.componentOptions
+  const menuSlot = slots?.default?.filter((v: VNode) => v.componentOptions?.tag === DropdownMenuName)?.[0]?.componentOptions
     || slots?.dropdown?.[0]?.componentOptions;
   return computed(() => {
     if (props.options && props.options.length > 0) return props.options;
