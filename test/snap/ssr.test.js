bd39cc6557970838d177500c65a431b3b25b78a4
azhe <xiaosansiji@gmail.com>
2022-10-29 18:38:44 +0800

    Feat/migrate2vitest (#1687)
    * feat: add vitest config
    
    * feat: remove jest dependencies
    
    * feat: rm jest config
    
    * fix: eslint error in test
    
    * feat: jest to vitest
    
    * feat: console in test
    
    * test: update snapshot
    
    * fix: unit test component mount
    
    * chore: rm useless js snapshot
    
    * feat: use istanbul collect coverage
    
    * fix: ci error
    
    * feat: add demo snapshot test script
    
    * test: add csr test snapshot
    
    * feat: add ssr test config
    
    * feat: remove useless dependency
    
    * test: remove independent demo test file

---
+++ test/snap/ssr.test.js
@@ -0,0 +1,36 @@
+import glob from 'glob';
+import { mount, config } from '@vue/test-utils';
+import { createRenderer } from 'vue-server-renderer';
+
+const transitionStub = () => ({
+  render: (h) => h('div'),
+});
+config.stubs.transition = transitionStub();
+
+function ssrSnapshotTest() {
+  const files = glob.sync('./src/**/_example/*.vue');
+  describe('ssr snapshot test', () => {
+    beforeAll(() => {
+      vi.useFakeTimers().setSystemTime(new Date('2021-12-31').getTime());
+    });
+    files.forEach((file) => {
+      if (file.indexOf('temp') > -1) {
+        return;
+      }
+      it(`renders ${file} correctly`, async () => {
+        const demo = await import(`../.${file}`);
+        const realDemoComp = demo.default ? demo.default : demo;
+        const wrapper = mount(realDemoComp);
+        const renderer = createRenderer();
+        renderer.renderToString(wrapper.vm, (err, str) => {
+          if (err) {
+            throw err;
+          }
+          expect(str).toMatchSnapshot();
+        });
+      }, 2000);
+    });
+  });
+}
+
+ssrSnapshotTest();
