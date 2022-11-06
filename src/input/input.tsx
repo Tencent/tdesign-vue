d9984f5c0fea64163a69dbf138c038f980e29a97
chaishi <974383157@qq.com>
2022-11-06 14:32:00 +0800

    fix(input-number): empty element

--- src/input/input.tsx
+++ src/input/input.tsx
@@ -209,7 +209,7 @@ export default mixins(getConfigReceiverMixins<InputInstance, InputConfig>('input
     // 当元素默认为 display: none 状态，无法提前准确计算宽度，因此需要监听元素宽度变化。比如：Tabs 场景切换。
     addTableResizeObserver(element: Element) {
       // IE 11 以下使用设置 minWidth 兼容；IE 11 以上使用 ResizeObserver
-      if (typeof window.ResizeObserver === 'undefined') return;
+      if (typeof window.ResizeObserver === 'undefined' || !element) return;
       this.resizeObserver = new window.ResizeObserver(() => {
         this.updateInputWidth();
       });
