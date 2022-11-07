7590879760a3d50ea82815536bdfd2a602910e21
omnis <39907007+aomnisz@users.noreply.github.com>
2022-11-07 13:16:54 +0800

    fix(alert): 警告框 demo 的宽度根据屏幕宽度自动撑开 (#1658)

--- src/alert/_example/title.vue
+++ src/alert/_example/title.vue
@@ -1,5 +1,5 @@
 <template>
-  <t-space>
+  <t-space style="width: 100%">
     <t-alert theme="info" title="这是一条普通的消息提示" message="这是与普通的消息提示相关的文字辅助说明" close>
       <template #operation>
         <span @click="handleOperation">相关操作</span>
