fa511215998463d4dc7617ca6b00958a732009b3
Uyarn <uyarnchen@gmail.com>
2022-11-09 15:56:25 +0800

    fix: fix form type

--- src/form/props.ts
+++ src/form/props.ts
@@ -16,10 +16,7 @@ export default {
     default: () => ({}),
   },
   /** 是否禁用整个表单 */
-  disabled: {
-    type: Boolean,
-    default: undefined,
-  },
+  disabled: Boolean,
   /** 表单错误信息配置，示例：`{ idcard: '请输入正确的身份证号码', max: '字符长度不能超过 ${max}' }` */
   errorMessage: {
     type: Object as PropType<TdFormProps['errorMessage']>,
@@ -57,10 +54,7 @@ export default {
     default: true,
   },
   /** 是否显示必填符号（*），默认显示 */
-  requiredMark: {
-    type: Boolean,
-    default: undefined,
-  },
+  requiredMark: Boolean,
   /** 重置表单的方式，值为 empty 表示重置表单为空，值为 initial 表示重置表单数据为初始值 */
   resetType: {
     type: String as PropType<TdFormProps['resetType']>,
@@ -77,7 +71,6 @@ export default {
   /** 表单校验不通过时，是否自动滚动到第一个校验不通过的字段，平滑滚动或是瞬间直达。值为空则表示不滚动 */
   scrollToFirstError: {
     type: String as PropType<TdFormProps['scrollToFirstError']>,
-    default: '' as TdFormProps['scrollToFirstError'],
     validator(val: TdFormProps['scrollToFirstError']): boolean {
       if (!val) return true;
       return ['', 'smooth', 'auto'].includes(val);
@@ -91,7 +84,6 @@ export default {
   /** 校验状态图标，值为 `true` 显示默认图标，默认图标有 成功、失败、警告 等，不同的状态图标不同。`statusIcon` 值为 `false`，不显示图标。`statusIcon` 值类型为渲染函数，则可以自定义右侧状态图标 */
   statusIcon: {
     type: [Boolean, Function] as PropType<TdFormProps['statusIcon']>,
-    default: undefined,
   },
   /** 【讨论中】当校验结果只有告警信息时，是否触发 `submit` 提交事件 */
   submitWithWarningMessage: Boolean,
