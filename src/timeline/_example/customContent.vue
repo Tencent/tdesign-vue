c6ca69b53fa98b5a3031ece94a907baffb7089ff
Uyarn <uyarnchen@gmail.com>
2022-11-09 16:50:05 +0800

    chore: update demo

--- src/timeline/_example/customContent.vue
+++ src/timeline/_example/customContent.vue
@@ -3,33 +3,27 @@
     <t-timeline mode="same">
       <t-timeline-item label="2022-01-01">
         <div>事件一</div>
-        <div :style="CommonStyle">事件一自定义内容</div>
+        <div class="timeline-custom-content">事件一自定义内容</div>
       </t-timeline-item>
       <t-timeline-item label="2022-02-01">
         <div>事件二</div>
-        <div :style="CommonStyle">事件二自定义内容</div>
+        <div class="timeline-custom-content">事件二自定义内容</div>
       </t-timeline-item>
       <t-timeline-item label="2022-03-01">
         <div>事件三</div>
-        <div :style="CommonStyle">事件三自定义内容</div>
+        <div class="timeline-custom-content">事件三自定义内容</div>
       </t-timeline-item>
       <t-timeline-item label="2022-04-01">
         <div>事件四</div>
-        <div :style="CommonStyle">事件四自定义内容</div>
+        <div class="timeline-custom-content">事件四自定义内容</div>
       </t-timeline-item>
     </t-timeline>
   </div>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      CommonStyle: {
-        fontSize: 12,
-        color: 'rgba(0,0,0,.6)',
-      },
-    };
-  },
-};
-</script>
-<style lang="scss" scoped></style>
+
+<style scoped>
+.timeline-custom-content {
+  font-size: 12px;
+  color: var(--td-text-color-secondary);
+}
+</style>
--- src/timeline/_example/customDot.vue
+++ src/timeline/_example/customDot.vue
@@ -52,4 +52,3 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped></style>
