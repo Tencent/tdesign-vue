3183f713fa42931def9ead58d671b6fa878b127d
azhe <xiaosansiji@gmail.com>
2022-11-15 16:51:05 +0800

    Fix/tag input autowidth (#1793)
    * fix: tag-input auto width not work
    
    * test: add drawer test
    
    * test: add drawer test
    
    * test: add input key down  test
    
    * feat: tag-input excessTagsDisplayType default value demo
    
    * test: update input-tag snapshot

--- src/tag-input/_example/excess.vue
+++ src/tag-input/_example/excess.vue
@@ -1,10 +1,10 @@
 <template>
   <t-space direction="vertical" style="width: 80%">
     <!-- 标签数量超出时，滚动显示 -->
-    <t-tag-input v-model="tags" label="Scroll: " clearable />
+    <t-tag-input v-model="tags" label="Scroll: " excess-tags-display-type="scroll" clearable />
 
     <!-- 标签数量超出时，换行显示 -->
-    <t-tag-input v-model="tags" label="BreakLine: " excess-tags-display-type="break-line" clearable />
+    <t-tag-input v-model="tags" label="BreakLine: " clearable />
   </t-space>
 </template>
 <script>
