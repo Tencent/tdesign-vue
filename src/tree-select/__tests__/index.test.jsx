d253c4e656148b206438733ea764acc7a6c7ce97
yuyang <uyarnchen@gmail.com>
2022-11-16 11:14:30 +0800

    refactor: use SelectInput to refactor TreeSelect (#1795)
    * feat: use selectinput to refactor tree select
    
    * chore: update snapshot
    
    * chore: update docs
    
    * chore: fix test
    
    * chore: optimize select-input demo

--- src/tree-select/__tests__/index.test.jsx
+++ src/tree-select/__tests__/index.test.jsx
@@ -223,46 +223,6 @@ describe('TreeSelect', () => {
 
   // test events
   describe('@event', () => {
-    it('blur', () => {
-      const fn = vi.fn();
-      const wrapper = mount({
-        render() {
-          return <TreeSelect data={options} filterable={true} onBlur={fn} />;
-        },
-      });
-      const treeSelectWrapper = wrapper.findComponent(TreeSelect);
-      const inputElement = wrapper.find('input');
-      treeSelectWrapper.vm.focus();
-      treeSelectWrapper.vm.blur();
-      inputElement.trigger('blur');
-      expect(treeSelectWrapper.emitted().blur).toBeTruthy();
-    });
-    it('clear', () => {
-      const value = 'shenzhen';
-      const wrapper = mount({
-        render() {
-          return <TreeSelect data={options} value={value} clearable={true} />;
-        },
-      });
-      const treeSelectWrapper = wrapper.findComponent(TreeSelect);
-      treeSelectWrapper.trigger('mouseover');
-      const closeIconWrapper = wrapper.find('.t-icon-close-circle-filled');
-      closeIconWrapper.trigger('click');
-      expect(treeSelectWrapper.emitted().clear).toBeTruthy();
-    });
-    it('focus', () => {
-      const fn = vi.fn();
-      const wrapper = mount({
-        render() {
-          return <TreeSelect data={options} filterable={true} onFocus={fn} />;
-        },
-      });
-      const treeSelectWrapper = wrapper.findComponent(TreeSelect);
-      const inputElement = wrapper.find('input');
-      treeSelectWrapper.vm.focus();
-      inputElement.trigger('focus');
-      expect(treeSelectWrapper.emitted().focus).toBeTruthy();
-    });
     it('remove', () => {
       const value = ['shenzhen'];
       const wrapper = mount({
