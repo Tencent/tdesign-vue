4b56f915ac8c58376fd377b45c69acc485c6c0ab
chaishi <974383157@qq.com>
2022-11-19 19:53:38 +0800

    fix(input-number): innerChange

--- src/input-number/useInputNumber.tsx
+++ src/input-number/useInputNumber.tsx
@@ -137,7 +137,7 @@ export default function useInputNumber(props: TdInputNumberProps, context: Setup
     const isNumberCode = ['-', '.', 'e', 'E'].includes(val.slice(-1));
     const smallNumber = val === '' ? undefined : Number(val);
     const newVal = isNumberCode ? val : smallNumber;
-    if (!isNaN(Number(newVal)) || !newVal || !isNumberCode) {
+    if ((!isNaN(Number(newVal)) || !newVal) && !isNumberCode) {
       setTValue(newVal, { type: 'input', e: ctx.e });
       userInput.value = newVal ? String(newVal) : '';
     } else if (isNumberCode) {
