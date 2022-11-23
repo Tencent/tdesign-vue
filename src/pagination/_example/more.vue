ed7743437da6e7d87c1479210bc0795e961d8630
Ccppmmm <110590735+Ccppmmm@users.noreply.github.com>
2022-11-23 10:41:51 +0800

    feat: pagination添加space (#1824)

--- src/pagination/_example/more.vue
+++ src/pagination/_example/more.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <span>展示首尾页码省略</span>
-    <t-pagination v-model="current" :total="100" :page-size.sync="pageSize" />
-    <span>不展示首尾页码省略</span>
-    <t-pagination v-model="current" page-ellipsis-mode="both-ends" :total="100" :page-size.sync="pageSize" />
+    <t-space direction="vertical" size="large" style="width: 100%">
+      <span>展示首尾页码省略</span>
+      <t-pagination v-model="current" :total="100" :page-size.sync="pageSize" />
+      <span>不展示首尾页码省略</span>
+      <t-pagination v-model="current" page-ellipsis-mode="both-ends" :total="100" :page-size.sync="pageSize" />
+    </t-space>
   </div>
 </template>
 
