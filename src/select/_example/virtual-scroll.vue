ec7eb9bbeaa5bac2b6ce6388dae760478adba5da
krist <66660723@qq.com>
2022-12-30 15:25:33 +0800

    fix(select): 修复 option slots 部分情况下异常渲染更新的问题

--- src/select/_example/virtual-scroll.vue
+++ src/select/_example/virtual-scroll.vue
@@ -1,13 +1,25 @@
 <template>
   <!-- 开启虚拟滚动请为select的panel设定好height 通过popupProps进行透传 -->
-  <t-select
-    v-model="value"
-    :options="options"
-    placeholder="请选择"
-    :style="{ width: '300px' }"
-    :scroll="{ type: 'virtual' }"
-    :popup-props="{ overlayInnerStyle: { height: '300px' } }"
-  />
+  <t-space>
+    <t-select
+      v-model="value1"
+      :options="options"
+      placeholder="请选择"
+      :style="{ width: '300px' }"
+      :scroll="{ type: 'virtual' }"
+      :popup-props="{ overlayInnerStyle: { height: '300px' } }"
+    />
+    <!-- 开启虚拟滚动请为select的panel设定好height 通过popupProps进行透传 -->
+    <t-select
+      v-model="value2"
+      placeholder="请选择"
+      :style="{ width: '300px' }"
+      :scroll="{ type: 'virtual' }"
+      :popup-props="{ overlayInnerStyle: { height: '300px' } }"
+    >
+      <t-option v-for="item in options" :value="item.value" :label="item.label" :key="item.value"></t-option>
+    </t-select>
+  </t-space>
 </template>
 
 <script lang="jsx">
@@ -18,7 +30,8 @@ export default {
       options.push({ label: `选项${i + 1}`, value: String(i) });
     }
     return {
-      value: '',
+      value1: '',
+      value2: '',
       options,
     };
   },
