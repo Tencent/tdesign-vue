73cf442a2c1aa7660a8940d93446a26466ff47df
chaishi <974383157@qq.com>
2023-01-14 12:07:03 +0800

    test: update snapshots

--- src/auto-complete/__tests__/mount.jsx
+++ src/auto-complete/__tests__/mount.jsx
@@ -1,6 +1,9 @@
 import { mount } from '@vue/test-utils';
 import { createElementById } from '../../../test/utils';
 
+// Vue2 触发 focus 等特殊事件，需要 attachTo。
+// trigger 文档：https://v1.test-utils.vuejs.org/api/wrapper/#trigger
+// attachTo 文档：https://v1.test-utils.vuejs.org/api/options.html#attachto
 export function getNormalAutoCompleteMount(AutoComplete, propsData = {}, listeners) {
   const options = [
     'FirstKeyword',
@@ -13,12 +16,8 @@ export function getNormalAutoCompleteMount(AutoComplete, propsData = {}, listene
     'ThirdKeyword',
   ];
 
-  // Vue2 触发 focus 等特殊事件，需要 attachTo。
-  // trigger 文档：https://v1.test-utils.vuejs.org/api/wrapper/#trigger
-  // attachTo 文档：https://v1.test-utils.vuejs.org/api/options.html#attachto
   const id = propsData.vue2AttachTo || 'auto-complete-test';
   createElementById(id);
-
   // eslint-disable-next-line
   delete propsData.vue2AttachTo;
 
