e86ebce82c7fb2110992e24d244a610374b95915
Uyarn <uyarnchen@gmail.com>
2023-01-17 14:00:21 +0800

    fix(timepicker): register panel component

--- src/time-picker/index.ts
+++ src/time-picker/index.ts
@@ -2,17 +2,18 @@ import VueCompositionAPI from '@vue/composition-api';
 import _TimePicker from './time-picker';
 import _TimeRangePicker from './time-range-picker';
 import _TimePickerPanel from './panel/time-picker-panel';
-
+import { TdTimePickerProps, TdTimeRangePickerProps } from './type';
 import withInstall from '../utils/withInstall';
 
 import './style';
 
 export * from './interface';
 
+export type TimePickerProps = TdTimePickerProps;
+export type TimeRangePickerProps = TdTimeRangePickerProps;
+
 export const TimePicker = withInstall(_TimePicker, VueCompositionAPI);
-
 export const TimeRangePicker = withInstall(_TimeRangePicker, VueCompositionAPI);
-
 export const TimePickerPanel = withInstall(_TimePickerPanel, VueCompositionAPI);
 
 export default TimePicker;
