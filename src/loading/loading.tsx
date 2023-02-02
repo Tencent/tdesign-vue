04c5feb5e26f16119c72b5f8791a7000fea49c94
chaishi <974383157@qq.com>
2023-02-02 11:47:16 +0800

    fix(loading): null

--- src/loading/loading.tsx
+++ src/loading/loading.tsx
@@ -168,13 +168,11 @@ export default mixins(classPrefixMixins).extend({
     }
 
     // Normal Loading without overlay or content
-    return (
-      this.loading && (
-        <div class={this.normalClasses} style={this.styles}>
-          {indicator}
-          {text}
-        </div>
-      )
-    );
+    return this.loading ? (
+      <div class={this.normalClasses} style={this.styles}>
+        {indicator}
+        {text}
+      </div>
+    ) : null;
   },
 });
