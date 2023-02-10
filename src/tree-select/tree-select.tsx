fbe76532c62385c1e699516fba2ed8b6a47141e8
sheepluo <yaoyanhuoyi@qq.com>
2023-02-10 16:52:55 +0800

    [TreeSelect] support tree.keys (#2128)
    * fix(tree-select): fix treeProps
    
    * fix(tree-select): .keys
    
    * test(tree-select): add keys and treeProps.keys unit tests

--- src/tree-select/tree-select.tsx
+++ src/tree-select/tree-select.tsx
@@ -70,8 +70,9 @@ export default defineComponent({
           {!this.loading ? (
             <Tree
               ref="treeRef"
+              key={this.treeKey}
               props={{
-                key: this.treeKey,
+                keys: this.tKeys,
                 value: [...this.multipleChecked],
                 actived: this.singleActivated,
                 hover: true,
