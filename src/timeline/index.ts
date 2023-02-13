e6a7ff3d486f365d3a6e8a3f8f94f1efbb47998d
chaishi <974383157@qq.com>
2023-02-13 20:08:41 +0800

    feat(timeline): tests & features & bugs

--- src/timeline/index.ts
+++ src/timeline/index.ts
@@ -2,13 +2,24 @@ import VueCompositionAPI from '@vue/composition-api';
 
 import _Timeline from './timeline';
 import _TimelineItem from './timeline-item';
-import { TdTimeLineProps, TdTimeLineItemProps } from './type';
+import { TdTimelineProps, TdTimelineItemProps } from './type';
 import withInstall from '../utils/withInstall';
 
 import './style/index.js';
 
-export type TimelineProps = TdTimeLineProps;
-export type TimelineItemProps = TdTimeLineItemProps;
+export type TimelineProps = TdTimelineProps;
+export type TimelineItemProps = TdTimelineItemProps;
+
+/**
+ * @deprecated use TimelineProps instead
+ */
+export type TdTimeLineProps = TimelineProps;
+
+/**
+ * @deprecated use TimelineItemProps instead
+ */
+export type TdTimeLineItemProps = TimelineItemProps;
+
 export * from './type';
 
 export const Timeline = withInstall(_Timeline, VueCompositionAPI);
