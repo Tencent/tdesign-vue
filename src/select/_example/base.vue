ce2961e80d2804503bb58b6ecf85e29fcc2a6c27
yuyang <uyarnchen@gmail.com>
2023-02-22 14:11:03 +0800

    feat: select and popup support scroll and title (#2176)
    * fix(select): display empty string
    
    * feat(popup): add scroll-to-bottom apis
    
    * chore: update snapshot

--- src/select/_example/base.vue
+++ src/select/_example/base.vue
@@ -19,7 +19,7 @@ export default {
       // 如果此处数据字段不是 label 和 value，而是 name 和 id，则可以传入参数 `keys` 定义别名
       // 示例： <t-select :options="options1" :keys="{ label: 'name', value: 'id' }" />
       options1: [
-        { label: '架构云', value: '1' },
+        { label: '架构云', value: '1', title: '架构云选项' },
         { label: '大数据', value: '2' },
         { label: '区块链', value: '3' },
         { label: '物联网', value: '4', disabled: true },
