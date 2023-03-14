1bddaff2cf0789f60a76301034f2349f8e454a04
HQ-Lin <a154571896@gmail.com>
2023-03-14 14:57:45 +0800

    fix: 修复类型检测问题

--- src/date-picker/_example/date-range.vue
+++ src/date-picker/_example/date-range.vue
@@ -1,14 +1,7 @@
 <template>
   <t-space direction="vertical">
     <t-date-range-picker allow-input clearable @pick="onPick" @change="onChange" />
-    <t-date-range-picker
-      format="YYYY-MM-DD HH:mm"
-      enable-time-picker
-      allow-input
-      clearable
-      @pick="onPick"
-      @change="onChange"
-    />
+    <t-date-range-picker enable-time-picker allow-input clearable @pick="onPick" @change="onChange" />
   </t-space>
 </template>
 
