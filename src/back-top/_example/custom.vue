40e8b450b9a9a4943ba5bc03f9f9e80257839cf7
Uyarn <uyarnchen@gmail.com>
2023-04-27 14:39:09 +0800

    chore: complete backtop

--- src/back-top/_example/custom.vue
+++ src/back-top/_example/custom.vue
@@ -2,8 +2,8 @@
   <t-space style="position: relative" direction="vertical">
     <t-space>
       <t-back-top style="position: relative" :visible-height="0" size="medium" :offset="[0, 0]"
-      ><span>返回顶部</span></t-back-top
-      >
+      ><t-icon name="top"
+      /></t-back-top>
       <t-back-top
         style="position: relative"
         :visible-height="0"
