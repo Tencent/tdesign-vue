d7142107e9763a3b9abdb4456ec5a5c807973890
yuyang <uyarnchen@gmail.com>
2023-04-27 16:34:50 +0800

    fix(space): slots is not reactive (#2372)
    * fix(space): slots is not reactive
    
    * chore: update snapshot

--- src/space/space.tsx
+++ src/space/space.tsx
@@ -8,7 +8,7 @@ export default defineComponent({
 
   props: { ...props },
 
-  setup(props, { slots }) {
+  setup(props) {
     const COMPONENT_NAME = usePrefixClass('space');
 
     const renderStyle = computed(() => {
@@ -47,21 +47,15 @@ export default defineComponent({
       COMPONENT_NAME,
       spaceClassNames,
       renderStyle,
-      slots,
     };
   },
   render() {
-    const {
-      COMPONENT_NAME, spaceClassNames, renderStyle, slots,
-    } = this;
-
-    const children = slots.default?.().filter((child) => child.tag !== undefined || child.text) || [];
+    const { COMPONENT_NAME, spaceClassNames, renderStyle } = this;
+    const children = this.$slots.default?.filter((child) => child.tag !== undefined || child.text) || [];
     const childCount = children?.length;
-
     const renderChildren = () => children.map((child, index) => {
       const separatorNode = renderTNodeJSX(this, 'separator');
       const showSeparator = index + 1 !== childCount && separatorNode;
-
       return [
           <div class={`${COMPONENT_NAME}-item`}>{child}</div>,
           showSeparator ? <div class={`${COMPONENT_NAME}-item-separator`}>{separatorNode}</div> : null,
