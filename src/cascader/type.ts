1c8e5072ce67dbf06d6f80bf1d80772099e24bd7
zekunwu <1430755343@qq.com>
2023-05-08 16:30:45 +0800

    fix: 恢复一部分出问题的API改动

--- src/cascader/type.ts
+++ src/cascader/type.ts
@@ -187,7 +187,7 @@ export interface TdCascaderProps<CascaderOption extends TreeOptionData = TreeOpt
   /**
    * 【开发中】自定义选中项呈现的内容
    */
-  valueDisplay?: string | TNode<{ value: SelectValue; onClose: (index: number) => void; displayValue?: SelectValue }>;
+  valueDisplay?: string | TNode<{ value: any; onClose: (index: number) => void; displayValue?: any }>;
   /**
    * 选中值模式。all 表示父节点和子节点全部会出现在选中值里面；parentFirst 表示当子节点全部选中时，仅父节点在选中值里面；onlyLeaf 表示无论什么情况，选中值仅呈现叶子节点
    * @default onlyLeaf
@@ -201,7 +201,7 @@ export interface TdCascaderProps<CascaderOption extends TreeOptionData = TreeOpt
   /**
    * 当输入框失去焦点时触发
    */
-  onBlur?: (context: { value: CascaderValue<CascaderOption> } & SelectInputBlurContext) => void;
+  onBlur?: (context: { value: CascaderValue<CascaderOption>; e: FocusEvent }) => void;
   /**
    * 选中值发生变化时触发。TreeNodeModel 从树组件中导出。`context.node` 表示触发事件的节点，`context.source` 表示触发事件的来源
    */
