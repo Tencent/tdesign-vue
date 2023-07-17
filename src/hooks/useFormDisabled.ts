9246f1c16d55383c60d1c552ff9982d759eb2c6c
sheepluo <yaoyanhuoyi@qq.com>
2023-07-17 18:30:33 +0800

    [Checkbox] Performance Optimization (#2583)
    * refactor(checkbox): code
    
    * feat(checkbox): performance
    
    * feat(checkbox): checked & disabled performance
    
    * feat(checkbox): performance
    
    * feat(checkbox): update snapshots and fix test bug
    
    * fix: lint error
    
    * feat(checkbox): add useKeyboardEvent
    
    * test(checkbox): update snapshots
    
    * chore: remove useless code
    
    * test: revert transfer test
    
    * feat(checkbox): support options and slots
    
    * test(checkbox): update snapshots
    
    * feat(checkbox): checkbox add lazyload to checkboxgroup
    
    * feat(checkbox): slot data

--- src/hooks/useFormDisabled.ts
+++ src/hooks/useFormDisabled.ts
@@ -1,8 +1,9 @@
 import { computed, getCurrentInstance } from '@vue/composition-api';
 
-export default function useFormDisabled() {
+export function useFormDisabled() {
   const formDisabled = computed(() => {
     const currentInstance = getCurrentInstance();
+    if (!currentInstance) return null;
     let { parent } = currentInstance;
     while (parent) {
       if (parent.proxy.$options.name === 'TForm') {
@@ -14,3 +15,5 @@ export default function useFormDisabled() {
   });
   return { formDisabled };
 }
+
+export default useFormDisabled;
