5db7f6e7e176af9d2766344a0b5bc75cfaa5730b
TabSpace <pillar0514@163.com>
2023-07-17 10:51:19 +0800

    fix(tree): tree 组件，解决结构改造后，非虚拟滚动模式未能缓存已创建节点的问题

--- src/tree/hooks/useTreeNodes.tsx
+++ src/tree/hooks/useTreeNodes.tsx
@@ -16,18 +16,39 @@ export default function useTreeNodes(props: TreeProps, context: SetupContext, st
 
   const { handleClick, handleChange } = useTreeEvents(props, context, state);
   const nodesEmpty = ref(false);
+  // 用于存储已呈现节点的缓存
+  const cacheMap = new Map();
 
   const refresh = () => {
-    let list = [];
+    let list: TypeTreeNode[] = [];
     const allNodes = store.getNodes();
     const isVirtual = virtualConfig?.isVirtualScroll.value;
     if (isVirtual) {
       // 虚拟滚动只渲染可见节点
       list = virtualConfig.visibleData.value;
+      nodesEmpty.value = list.length <= 0;
     } else {
-      list = allNodes.filter((node: TypeTreeNode) => node.visible);
+      // 非虚拟滚动，缓存曾经展示过的节点
+      let hasVisibleNode = false;
+      allNodes.forEach((node: TypeTreeNode) => {
+        if (node.visible) {
+          // 曾经展示过的节点加入缓存，避免再次创建
+          hasVisibleNode = true;
+          cacheMap.set(node.value, node.value);
+        }
+        if (cacheMap.get(node.value)) {
+          // 创建的节点是缓存的节点
+          list.push(node);
+        }
+      });
+      nodesEmpty.value = !hasVisibleNode;
+      cacheMap.forEach((value) => {
+        // 在缓存中清理结构变化后不存在的节点
+        if (!store.getNode(value)) {
+          cacheMap.delete(value);
+        }
+      });
     }
-    nodesEmpty.value = list.length <= 0;
     // 渲染为平铺列表
     nodes.value = list;
   };
