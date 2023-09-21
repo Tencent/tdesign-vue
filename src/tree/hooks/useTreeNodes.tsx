024c4444a0011b1b89cf325aa2b39bd91c28cd62
TabSpace <pillar0514@163.com>
2023-09-21 16:00:05 +0800

    refactor(tree): tree 组件，解决虚拟滚动数据变更的状态异常

--- src/tree/hooks/useTreeNodes.tsx
+++ src/tree/hooks/useTreeNodes.tsx
@@ -1,5 +1,5 @@
 import {
-  ref, TypeCreateElement, privateKey, TypeVNode,
+  ref, watch, TypeCreateElement, privateKey, TypeVNode,
 } from '../adapt';
 import { TypeTreeRow, TypeTreeNode, TypeTreeState } from '../tree-types';
 import TreeItem from '../tree-item';
@@ -8,7 +8,7 @@ import useTreeEvents from './useTreeEvents';
 // tree 节点列表渲染
 export default function useTreeNodes(state: TypeTreeState) {
   const {
-    store, scope, nodes, virtualConfig,
+    store, scope, allNodes, nodes, virtualConfig,
   } = state;
   const { handleClick, handleChange } = useTreeEvents(state);
   const nodesEmpty = ref(false);
@@ -16,37 +16,43 @@ export default function useTreeNodes(state: TypeTreeState) {
   const cacheMap = new Map();
 
   const refresh = () => {
-    let list: TypeTreeNode[] = [];
-    const allNodes = store.getNodes();
-    const isVirtual = virtualConfig?.isVirtualScroll.value;
-    if (isVirtual) {
-      // 虚拟滚动只渲染可见节点
-      list = virtualConfig.visibleData.value;
-      nodesEmpty.value = list.length <= 0;
-    } else {
-      // 非虚拟滚动，缓存曾经展示过的节点
-      let hasVisibleNode = false;
-      allNodes.forEach((node: TypeTreeNode) => {
-        if (node.visible) {
-          // 曾经展示过的节点加入缓存，避免再次创建
-          hasVisibleNode = true;
-          cacheMap.set(node.value, node.value);
-        }
-        if (cacheMap.get(node.value)) {
-          // 创建的节点是缓存的节点
-          list.push(node);
-        }
-      });
-      nodesEmpty.value = !hasVisibleNode;
-      cacheMap.forEach((value) => {
-        // 在缓存中清理结构变化后不存在的节点
-        if (!store.getNode(value)) {
-          cacheMap.delete(value);
-        }
-      });
-    }
+    allNodes.value = store.getNodes();
+  };
+
+  const refreshVisibleNodes = () => {
+    const list: TypeTreeNode[] = [];
+    // 非虚拟滚动，缓存曾经展示过的节点
+    let hasVisibleNode = false;
+    allNodes.value.forEach((node: TypeTreeNode) => {
+      if (node.visible) {
+        // 曾经展示过的节点加入缓存，避免再次创建
+        hasVisibleNode = true;
+        cacheMap.set(node.value, node.value);
+      }
+      if (cacheMap.get(node.value)) {
+        // 创建的节点是缓存的节点
+        list.push(node);
+      }
+    });
+    cacheMap.forEach((value) => {
+      // 在缓存中清理结构变化后不存在的节点
+      if (!store.getNode(value)) {
+        cacheMap.delete(value);
+      }
+    });
     // 渲染为平铺列表
     nodes.value = list;
+    nodesEmpty.value = !hasVisibleNode;
+  };
+
+  const refreshVirtualNodes = () => {
+    let list: TypeTreeNode[] = [];
+    const isVirtual = virtualConfig?.isVirtualScroll.value;
+    if (!isVirtual) return;
+    // 虚拟滚动只渲染可见节点
+    list = virtualConfig.visibleData.value;
+    nodes.value = list;
+    nodesEmpty.value = list.length <= 0;
   };
 
   // 创建单个 tree 节点
@@ -78,7 +84,13 @@ export default function useTreeNodes(state: TypeTreeState) {
     return treeNodeViews;
   };
 
-  refresh();
+  watch(allNodes, () => {
+    const isVirtual = virtualConfig?.isVirtualScroll.value;
+    if (!isVirtual) {
+      refreshVisibleNodes();
+    }
+  });
+  watch(virtualConfig.visibleData, refreshVirtualNodes);
   store.emitter.on('update', refresh);
 
   return {
