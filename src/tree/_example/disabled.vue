d048f82440fcf0ec434a4c983eaa29e6615f4aa0
TabSpace <pillar0514@163.com>
2023-11-24 18:25:43 +0800

    test(tree): tree 组件，完善 disabled 相关示例

--- src/tree/_example/disabled.vue
+++ src/tree/_example/disabled.vue
@@ -26,12 +26,12 @@
     >
       <template #operations="{ node }">
         <t-space :size="10">
-          <t-button v-if="node.disabled" size="small" theme="success" variant="base" @click="setEnable(node)"
-          >enable</t-button
-          >
-          <t-button v-if="!node.disabled" size="small" theme="warning" variant="base" @click="setDisable(node)"
-          >disable</t-button
-          >
+          <t-button v-if="node.disabled" size="small" theme="success" variant="base" @click="setEnable(node)">
+            enable
+          </t-button>
+          <t-button v-if="!node.disabled" size="small" theme="warning" variant="base" @click="setDisable(node)">
+            disable
+          </t-button>
         </t-space>
       </template>
     </t-tree>
@@ -40,8 +40,9 @@
 
 <script>
 // 预期规则:
-// 父节点被禁用，所有子节点一并呈现禁用状态，除非 checkStrictly = true
-// 父节点操作，不影响被禁用的子节点的原始选中状态。
+// 默认父节点被禁用，所有子节点一并呈现禁用状态
+// checkStrictly = true 时，父节点禁用状态不影响子节点禁用状态。
+// 父节点操作选中，不影响被禁用的子节点的原始选中状态。
 // 子节点被禁用且未选中，父节点半选状态再次点击可切换为未选中状态。
 
 export default {
