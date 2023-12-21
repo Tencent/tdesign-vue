dca306c51c4929cf8426f2a990edc8cd01c5b03c
Uyarn <uyarnchen@gmail.com>
2023-12-21 15:38:09 +0800

    chore: update snapshot

--- src/card/_example/custom-loading-props.vue
+++ src/card/_example/custom-loading-props.vue
@@ -13,7 +13,7 @@ export default {
       customProps: {
         text: 'TDesign努力加载中...',
       },
-      infoMessage: '',
+      infoMessage: '卡片内容，以描述性为主，可以是文字、图片或图文组合的形式。按业务需求进行自定义组合。',
     };
   },
 };
