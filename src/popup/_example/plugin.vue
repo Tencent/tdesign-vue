a35dbd4287270e3b5490c63f8c56d0a7ef101770
wū yāng <uyarnchen@gmail.com>
2024-01-02 17:52:02 +0800

    docs: update popup plugin demo (#3014)

--- src/popup/_example/plugin.vue
+++ src/popup/_example/plugin.vue
@@ -1,8 +1,15 @@
 <template>
-  <t-space>
-    <t-button variant="outline" @click="handleElement1" class="trigger-element1">已渲染的节点1</t-button>
-    <t-button variant="outline" @click="handleElement2" class="trigger-element2">已渲染的节点2</t-button>
-    <t-button variant="outline" @click="handleElement3" class="trigger-element3">已渲染的节点3</t-button>
+  <t-space direction="vertical">
+    <t-button variant="outline" @click="handleElement1" class="trigger-element1">正常的方式打开</t-button>
+    <t-button variant="outline" @click="handleElement2" class="trigger-element2"
+    >通过Plugin打开，并修改不同浮层的配置</t-button
+    >
+    <div>
+      <span>这里是一个日志查询的例子，在很长的日志内容中，日志内容存在换行的情况，可以点击链接进行日志查询操作</span>
+      <a class="trigger-element3" style="color: var(--td-text-color-brand)" @click="handleCreatePopupOffset"
+      >点击此链接，会打开浮层进行跳转操作</a
+      >
+    </div>
   </t-space>
 </template>
 
@@ -12,27 +19,49 @@ import { PopupPlugin } from 'tdesign-vue';
 export default {
   methods: {
     handleElement1() {
-      this.$popup('.trigger-element1', '渲染文本内容', {
-        placement: 'bottom',
-        showArrow: true,
-        trigger: 'hover',
-        destroyOnClose: true,
-      });
+      this.$popup('.trigger-element1', '渲染文本的内容');
     },
     handleElement2() {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      this.$popup('.trigger-element2', (h) => <div>渲染的是DOM节点</div>, {
-        placement: 'top',
-        trigger: 'click',
+      PopupPlugin('.trigger-element2', '渲染文本的内容', {
+        placement: 'right',
+        showArrow: false,
       });
     },
-    handleElement3() {
-      PopupPlugin('.trigger-element3', '渲染文本内容', {
-        placement: 'right',
-        showArrow: true,
-        trigger: 'hover',
+    handleCreatePopupOffset() {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      this.$popup('.trigger-element3', (h) => <div>透传popperOptions，在offset里控制节点位置</div>, {
+        placement: 'bottom',
+        popperOptions: {
+          modifiers: [
+            {
+              name: 'offset',
+              trigger: 'click',
+              options: {
+                offset: ({ reference }) => {
+                  const target = document.querySelector('.trigger-element2');
+                  let { lineHeight } = getComputedStyle(target);
+                  if (lineHeight === 'normal') {
+                    const temp = document.createElement('div');
+                    temp.innerText = 't';
+                    document.body.appendChild(temp);
+                    lineHeight = `${temp.offsetHeight}px`;
+                    document.body.removeChild(temp);
+                  }
+                  const isBreakLine = reference.height > parseInt(lineHeight, 10);
+                  return isBreakLine ? [reference.x, -reference.height + 10] : [0, 0];
+                },
+              },
+            },
+          ],
+        },
       });
     },
   },
 };
 </script>
+<style scoped>
+.trigger-element3 {
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>
