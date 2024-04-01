5f25d9fcae4d1dc7cb8c9709ac5613451264607f
wū yāng <uyarnchen@gmail.com>
2024-04-01 16:42:10 +0800

    chore: fix lint

--- src/date-picker/DatePicker.tsx
+++ src/date-picker/DatePicker.tsx
@@ -1,6 +1,7 @@
 import { defineComponent, watch, computed } from '@vue/composition-api';
 import dayjs from 'dayjs';
 import { CalendarIcon as TdCalendarIcon } from 'tdesign-icons-vue';
+import isDate from 'lodash/isDate';
 
 import { usePrefixClass } from '../hooks/useConfig';
 import { useGlobalIcon } from '../hooks/useGlobalIcon';
@@ -51,12 +52,11 @@ export default defineComponent({
     const isDisabled = computed(() => formDisabled.value || props.disabled);
 
     watch(popupVisible, (visible) => {
-      const dateValue =
-        // Date 属性不再 parse，避免 dayjs 处理成 Invalid
-        value.value && !isDate(value.value)
-          ? covertToDate(value.value as string, formatRef.value?.valueType)
-          : value.value;
-      
+      // Date valueType add empty string don't need to be parsed
+      const dateValue = value.value && !isDate(value.value)
+        ? covertToDate(value.value as string, formatRef.value?.valueType)
+        : value.value;
+
       cacheValue.value = formatDate(dateValue, {
         format: formatRef.value.format,
       });
