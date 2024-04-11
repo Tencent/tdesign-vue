2b122255eee3e0b1abb8bf1b2ca669df84f5d112
wū yāng <uyarnchen@gmail.com>
2024-04-11 14:52:26 +0800

    chore: rename loading directive export (#3140)

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import * as components from './components';
 function install(Vue: VueConstructor, config?: object) {
   Object.keys(components).forEach((key) => {
     if (components[key]) {
+      if (/directive/i.test(key)) return;
       /plugin/i.test(key) ? Vue.use(components[key]) : Vue.use(components[key], config);
     }
   });
