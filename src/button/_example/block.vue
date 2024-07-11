f3f91eab0fdb1f6b271197803e74f27f7ed13429
liweijie0812 <674416404@qq.com>
2024-07-11 21:04:49 +0800

    test: ci

--- src/button/_example/block.vue
+++ src/button/_example/block.vue
@@ -1,6 +1,6 @@
 <template>
   <t-space direction="vertical" align="center">
-    <t-button block theme="primary" variant="base">填充按钮</t-button>
+    <t-button block theme="primary" variant="base">填充按钮123213213213</t-button>
     <t-button block variant="outline">描边按钮</t-button>
     <t-button block variant="dashed">虚框按钮</t-button>
     <t-button block variant="text">文字按钮</t-button>
