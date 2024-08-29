1e7fdcf7b473502569fe9d5fa083a9b7a2fcf8c1
Haixing <65376724+HaixingOoO@users.noreply.github.com>
2024-08-29 16:28:04 +0800

    feat(empty): add new component Empty (#3287)
    * feat(empty): new Empty component
    
    * fix(empty): fix render svg
    
    * perf(empty): optimization code
    
    * chore(settings): revert vscode settings file
    
    * chore(empty): rm overage file
    
    * chore: fix demo
    
    * chore: fix docs
    
    * chore: fix demo
    
    * chore: update snapshot
    
    * chore: fix test
    
    ---------
    
    Co-authored-by: Uyarn <uyarnchen@gmail.com>

---
+++ src/empty/_example/self-defined.vue
@@ -0,0 +1,34 @@
+<template>
+  <t-row :gutter="40">
+    <t-col>
+      <t-empty description="暂无数据">
+        <template #image>
+          <error-circle-icon :size="64" color="var(--td-text-color-placeholder)" />
+        </template>
+      </t-empty>
+    </t-col>
+    <t-col>
+      <t-empty description="暂无数据">
+        <template #image>
+          <div
+            :style="{
+              width: '64px',
+              height: '64px',
+              backgroundImage: 'url(https://tdesign.gtimg.com/demo/demo-image-1.png)',
+              backgroundSize: '100% 100%',
+            }"
+          ></div>
+        </template>
+      </t-empty>
+    </t-col>
+  </t-row>
+</template>
+<script>
+import { ErrorCircleIcon } from 'tdesign-icons-vue';
+
+export default {
+  components: {
+    ErrorCircleIcon,
+  },
+};
+</script>
