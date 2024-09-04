429672be22dc49aec1f7c0cd429c3cdfbcf216cf
liweijie0812 <674416404@qq.com>
2024-09-04 17:25:29 +0800

    fix(input): fix status default value (#3307)

--- src/input/props.ts
+++ src/input/props.ts
@@ -91,10 +91,10 @@ export default {
   },
   /** 是否开启拼写检查，HTML5 原生属性，[点击查看详情](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/spellcheck) */
   spellCheck: Boolean,
-  /** 输入框状态。默认情况会由组件内部根据实际情况呈现，如果文本过长引起的状态变化 */
+  /** 输入框状态 */
   status: {
     type: String as PropType<TdInputProps['status']>,
-    default: undefined as TdInputProps['status'],
+    default: 'default' as TdInputProps['status'],
     validator(val: TdInputProps['status']): boolean {
       if (!val) return true;
       return ['default', 'success', 'warning', 'error'].includes(val);
@@ -124,6 +124,7 @@ export default {
   /** 输入框的值 */
   value: {
     type: [String, Number] as PropType<TdInputProps['value']>,
+    default: undefined,
   },
   /** 输入框的值，非受控属性 */
   defaultValue: {
