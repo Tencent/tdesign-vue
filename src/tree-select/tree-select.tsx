888116711dcb6d470216ef60f2d8c66178725948
wū yāng <uyarnchen@gmail.com>
2024-09-23 10:53:01 +0800

    fix(tree-select): fix multiple select click parent node callback (#3330)

--- src/tree-select/tree-select.tsx
+++ src/tree-select/tree-select.tsx
@@ -106,7 +106,7 @@ export default defineComponent({
                 onLoad: this.treeNodeLoad,
                 onChange: this.treeNodeChange,
                 onActive: this.treeNodeActive,
-                expandOnClickNode: true,
+                expandOnClickNode: false,
                 empty: () => this.renderDefaultTNode('empty', {
                   defaultNode: <div class={`${this.classPrefix}-select__empty`}>{this.global.empty}</div>,
                 }),
