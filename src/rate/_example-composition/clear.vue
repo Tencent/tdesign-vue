56f16a3c122c69d27858bc5f883c2395eab5bf65
myron <myronliu347@gmail.com>
2024-09-24 19:25:17 +0800

    feat(Rate): add clearable prop (#3332)
    * fix(TimePicker): fixed only support hh:mm format
    
    * fix(TimePicker): disabled position only is  start
    
    * fix(Upload): fixed vue error on uploadPastedFiles is false
    
    * docs: add readonly in api
    
    * feat(Rate): add allowClear Prop
    
    * chore: update test snap
    
    * chore: change prop name clearable
    
    * chore: clearable result should be 0
    
    * chore: clearable result should be 0
    
    ---------
    
    Co-authored-by: wū yāng <uyarnchen@gmail.com>

---
+++ src/rate/_example-composition/clear.vue
@@ -0,0 +1,14 @@
+<template>
+  <t-space direction="vertical">
+    <h3>clearable: true</h3>
+    <t-rate v-model="value1" clearable />
+    <h3>clearable: false</h3>
+    <t-rate v-model="value2" />
+  </t-space>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const value1 = ref(3);
+const value2 = ref(3);
+</script>
---
+++ src/rate/_example/clear.vue
@@ -0,0 +1,18 @@
+<template>
+  <t-space direction="vertical">
+    <h3>clearable: true</h3>
+    <t-rate v-model="value1" clearable />
+    <h3>clearable: false</h3>
+    <t-rate v-model="value2" />
+  </t-space>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      value1: 3,
+      value2: 3,
+    };
+  },
+};
+</script>
