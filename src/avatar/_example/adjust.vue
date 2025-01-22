d03d986727662b20c781d937e465c46ed54beb54
黎伟杰 <674416404@qq.com>
2025-01-22 22:12:19 +0800

    ci: test

--- src/avatar/_example/adjust.vue
+++ src/avatar/_example/adjust.vue
@@ -1,7 +1,7 @@
 <template>
   <t-space size="large">
     <t-avatar>王11111</t-avatar>
-    <t-avatar>王亿</t-avatar>
+    <t-avatar>王亿222</t-avatar>
     <t-avatar>王亿亿</t-avatar>
   </t-space>
 </template>
