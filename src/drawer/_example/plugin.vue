3e5df4bb50f328b5cb20320f9969c2d8be43f6ca
Wesley <985189328@qq.com>
2025-02-23 22:44:39 +0800

    feat: drawerPlugin

---
+++ src/drawer/_example/plugin.vue
@@ -0,0 +1,31 @@
+<template>
+  <t-space break-line>
+    <t-button theme="primary" @click="onDrawerPlugin">DrawerPlugin</t-button>
+  </t-space>
+</template>
+<script>
+import { DrawerPlugin } from 'tdesign-vue';
+
+export default {
+  methods: {
+    onDrawerPlugin() {
+      const confirmDia = DrawerPlugin({
+        header: 'Drawer-Confirm-Plugin',
+        body: '你确定要删除么?',
+        confirmBtn: '确定',
+        cancelBtn: '取消',
+        onConfirm: ({ e }) => {
+          console.log('confirm button has been clicked!');
+          console.log('e: ', e);
+          confirmDia.hide();
+        },
+        onClose: ({ e, trigger }) => {
+          console.log('e: ', e);
+          console.log('trigger: ', trigger);
+          confirmDia.hide();
+        },
+      });
+    },
+  },
+};
+</script>
---
+++ src/drawer/plugin.tsx
@@ -0,0 +1,73 @@
+import Vue from 'vue';
+import DrawerComponent from './drawer';
+
+import { getAttach } from '../utils/dom';
+import { globalConfigSymbol } from '../config-provider/config-provider';
+
+import type { DrawerOptions, DrawerMethod, DrawerInstance } from './type';
+
+const createDrawer: DrawerMethod = (props: DrawerOptions) => {
+  const options = { ...props };
+
+  const globalOptions = Vue.prototype[globalConfigSymbol];
+  const drawer = new DrawerComponent({
+    propsData: {
+      ...options,
+      onClose:
+        options.onClose
+        || (() => {
+          drawer.visible = false;
+        }),
+      instanceGlobal: globalOptions?.drawer,
+    },
+  }).$mount();
+  drawer.visible = true;
+  if (options.className) {
+    options.className.split(' ').forEach((name) => {
+      drawer.$el.classList.add(name.trim());
+    });
+  }
+  if (options.style) {
+    (drawer.$el as HTMLElement).style.cssText += options.style;
+  }
+  const container = getAttach(options.attach);
+  if (container) {
+    container.appendChild(drawer.$el);
+  } else {
+    console.error('attach is not exist');
+  }
+
+  const drawerNode: DrawerInstance = {
+    show: () => {
+      drawer.visible = true;
+    },
+    hide: () => {
+      drawer.visible = false;
+    },
+    update: (options: DrawerOptions) => {
+      Object.assign(drawer, options);
+    },
+    destroy: () => {
+      drawer.visible = false;
+      container.contains(drawer.$el) && container.removeChild(drawer.$el);
+    },
+  };
+  return drawerNode;
+};
+
+export type DrawerPluginType = Vue.PluginObject<undefined> & DrawerMethod;
+
+export const DrawerPlugin: DrawerPluginType = createDrawer as DrawerPluginType;
+
+DrawerPlugin.install = () => {
+  Vue.prototype.$drawer = createDrawer;
+};
+
+export default DrawerPlugin;
+
+declare module 'vue/types/vue' {
+  // Bind to `this` keyword
+  interface Vue {
+    $drawer: DrawerMethod;
+  }
+}
