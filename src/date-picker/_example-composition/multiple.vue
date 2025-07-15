c793c2b179b95dc1156f73dd92f1331068caab3f
betavs <betavs@qq.com>
2025-07-15 12:18:00 +0800

    fix(date-picker): handle tag removal for week and quarter modes

--- src/date-picker/_example-composition/multiple.vue
+++ src/date-picker/_example-composition/multiple.vue
@@ -1,6 +1,15 @@
 <template>
   <t-space direction="vertical">
-    <t-date-picker v-model="date" placeholder="可清除、可输入的日期选择器" clearable multiple @change="handleChange" />
+    <t-date-picker v-model="date" placeholder="支持日期的多选" clearable multiple @change="handleChange" />
+    <t-date-picker v-model="week" mode="week" placeholder="支持周的多选" clearable multiple @change="handleChange" />
+    <t-date-picker
+      v-model="quarter"
+      mode="quarter"
+      placeholder="支持季度的多选"
+      clearable
+      multiple
+      @change="handleChange"
+    />
   </t-space>
 </template>
 
@@ -8,9 +17,9 @@
 import { ref } from 'vue';
 
 const date = ref(['2000-01-04', '2000-01-03', '2000-01-05']);
+const week = ref([]);
+const quarter = ref([]);
 const handleChange = (value, context) => {
   console.log('onChange:', value, context);
-  console.log('timestamp:', context.dayjsValue.valueOf());
-  console.log('YYYYMMDD:', context.dayjsValue.format('YYYYMMDD'));
 };
 </script>
