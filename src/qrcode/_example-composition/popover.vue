a7e8f68678ed9c98a611b5b26433b71210cd51dd
Wesley <985189328@qq.com>
2025-08-07 15:43:38 +0800

    feat(QRCode): add QRCode Component (#3652)
    * feat: docs & type & prop
    
    * feat:  add QRCode
    
    * fix: test
    
    * fix: lint
    
    * chore: update snapshot
    
    * chore: p
    
    * chore: slot
    
    * chore: optimize
    
    * chore: optimize
    
    * chore: optimize
    
    * chore: optimize
    
    * fix: lint
    
    * chore: update snapshot
    
    * chore: update
    
    * chore: update coverage
    
    * chore: update common
    
    * chore: update demo
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: wū yāng <uyarnchen@gmail.com>

---
+++ src/qrcode/_example-composition/popover.vue
@@ -0,0 +1,10 @@
+<template>
+  <div style="width: 100%">
+    <t-popup trigger="hover">
+      <t-button>Hover me</t-button>
+      <template #content>
+        <t-qrcode value="https://tdesign.tencent.com/" :size="136" borderless />
+      </template>
+    </t-popup>
+  </div>
+</template>
