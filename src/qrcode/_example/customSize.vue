11552c31c7010d5d0587b3e4760423c37aadae06
Wesley <985189328@qq.com>
2025-08-07 15:43:38 +0800

    feat(QRCode): add QRCode Component (#3652)
    * feat: docs & type & prop
    
    * feat:  add QRCode
    
    * fix: test
    
    * fix: lint
    
    * chore: update snapshot
    
    * chore: p
    
    * chore: slot
    
    * chore: optimize
    
    * chore: optimize
    
    * chore: optimize
    
    * chore: optimize
    
    * fix: lint
    
    * chore: update snapshot
    
    * chore: update
    
    * chore: update coverage
    
    * chore: update common
    
    * chore: update demo
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: wū yāng <uyarnchen@gmail.com>

---
+++ src/qrcode/_example/customSize.vue
@@ -0,0 +1,43 @@
+<template>
+  <t-space direction="vertical">
+    <t-space>
+      <t-button :disabled="size <= minSize" @click="size -= 10">
+        <template #icon>
+          <minus-icon />
+        </template>
+        Smaller
+      </t-button>
+      <t-button :disabled="size >= maxSize" @click="size += 10">
+        <template #icon>
+          <add-icon />
+        </template>
+        Larger
+      </t-button>
+    </t-space>
+    <t-qrcode
+      level="H"
+      :size="size"
+      :icon-size="size / 4"
+      value="https://tdesign.tencent.com/"
+      icon="https://tdesign.gtimg.com/site/tdesign-logo.png"
+    />
+  </t-space>
+</template>
+
+<script>
+import { MinusIcon, AddIcon } from 'tdesign-icons-vue';
+
+export default {
+  components: {
+    MinusIcon,
+    AddIcon,
+  },
+  data() {
+    return {
+      minSize: 40,
+      maxSize: 300,
+      size: 160,
+    };
+  },
+};
+</script>
