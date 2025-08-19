8a4deb16ec878c6caef54a0f7fff90ab25d0c2b8
lwj <674416404@qq.com>
2025-08-19 23:32:14 +0800

    feat: migrateTreeSelect

--- script/migrate/components/tree-select.mjs
+++ script/migrate/components/tree-select.mjs
@@ -6,7 +6,7 @@ export default function migrateTreeSelect() {
     content = content.replace('...this.treeProps', '...(this.treeProps as TreeProps)').replace("import Tree from '../tree';", "import Tree, { TreeProps } from '../tree';");
   }
   if (content.includes('...this.selectInputProps')) {
-    content = content.replace('...this.selectInputProps', '...(this.selectInputProps as selectInputProps)').replace("import SelectInput from '../select-input';", "import SelectInput, { SelectInputProps } from '../select-input';");
+    content = content.replace('...this.selectInputProps', '...(this.selectInputProps as SelectInputProps)').replace("import SelectInput from '../select-input';", "import SelectInput, { SelectInputProps } from '../select-input';");
   }
   writeFileSync('src/tree-select/tree-select.tsx', content, 'utf8');
 }
