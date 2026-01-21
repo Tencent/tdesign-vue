782cd7b38cca61ed429d1679cc819c3c703680f3
Uyarn <uyarnchen@gmail.com>
2026-01-21 11:17:18 +0800

    chore: release 1.14.5-naruto

--- src/menu/head-menu.tsx
+++ src/menu/head-menu.tsx
@@ -138,7 +138,8 @@ export default defineComponent({
         <ul class={[`${this.classPrefix}-head-menu__submenu`, `${this.classPrefix}-submenu`]}>
           {
             <Tabs value={this.activeValue} onChange={this.handleTabChange}>
-              {this.submenu.map((item) => (
+              {/* 由于virtual child机制，这里通过vMenu getChild 会有两个相同的节点，故做此处理 */}
+              {this.submenu.slice(0, this.submenu.length / 2).map((item) => (
                 <TabPanel value={item.value} label={item.vnode[0].text}>
                   {item.children && item.children.length > 0
                     ? this.renderNormalSubmenu(item.children, depth + 1)
