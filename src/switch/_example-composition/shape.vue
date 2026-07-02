1cdeeb062b0871290a4e8b326286ec6598fff55b
Seeridia <i@seeridia.top>
2026-07-02 21:11:23 +0800

    feat(Switch): support shape prop

---
+++ src/switch/_example-composition/shape.vue
@@ -0,0 +1,13 @@
+<template>
+  <t-space>
+    <t-switch v-model="checked" shape="circle"></t-switch>
+    <t-switch v-model="checked" shape="round"></t-switch>
+    <t-switch v-model="checked" shape="line"></t-switch>
+  </t-space>
+</template>
+
+<script lang="ts" setup>
+import { ref } from '@vue/composition-api';
+
+const checked = ref(true);
+</script>
---
+++ src/switch/_example/shape.vue
@@ -0,0 +1,17 @@
+<template>
+  <t-space>
+    <t-switch v-model="checked" shape="circle"></t-switch>
+    <t-switch v-model="checked" shape="round"></t-switch>
+    <t-switch v-model="checked" shape="line"></t-switch>
+  </t-space>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      checked: true,
+    };
+  },
+};
+</script>
