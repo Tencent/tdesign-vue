f8e99464e4516e7e8d3ce1834829fc0fcf7f89cf
Uyarn <uyarnchen@gmail.com>
2026-07-29 14:07:51 +0800

    fix(Headmenu): fix head menu always display

--- src/menu/__tests__/head-menu.test.jsx
+++ src/menu/__tests__/head-menu.test.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable class-methods-use-this, no-param-reassign */
+/* eslint-disable class-methods-use-this, max-classes-per-file, no-param-reassign */
 import { mount } from '@vue/test-utils';
 import { HeadMenu, Menu, MenuItem } from '@/src/menu';
 
@@ -84,6 +84,52 @@ describe('HeadMenu', () => {
       expect(wrapper.find('.side-menu-content').element.style.display).toBe('');
       expect(wrapper.find('.header-menu-content').element.style.display).toBe('none');
       expect(wrapper.find('.t-head-menu__submenu--more').element.style.display).toBe('none');
+
+      wrapper.destroy();
+      window.ResizeObserver = NativeResizeObserver;
+    });
+
+    it('folds overflowing head menu items', async () => {
+      const NativeResizeObserver = window.ResizeObserver;
+      let resize;
+      window.ResizeObserver = class {
+        constructor(callback) {
+          resize = callback;
+        }
+
+        observe() {}
+
+        unobserve() {}
+
+        disconnect() {}
+      };
+
+      const wrapper = mount({
+        render() {
+          return (
+            <HeadMenu expandType="popup">
+              <MenuItem value="1">菜单一</MenuItem>
+              <MenuItem value="2">菜单二</MenuItem>
+            </HeadMenu>
+          );
+        },
+      });
+      await wrapper.vm.$nextTick();
+
+      const inner = wrapper.find('.t-head-menu__inner').element;
+      const items = wrapper.findAll('.t-head-menu > .t-head-menu__inner > .t-menu > .t-menu__item');
+      const more = wrapper.find('.t-head-menu__submenu--more');
+      Object.defineProperty(inner, 'clientWidth', { configurable: true, value: 180 });
+      items.wrappers.forEach((item) => {
+        item.element.getBoundingClientRect = () => ({ width: 100 });
+      });
+      more.element.getBoundingClientRect = () => ({ width: 40 });
+      resize([]);
+
+      expect(items.at(0).element.style.display).toBe('');
+      expect(items.at(1).element.style.display).toBe('none');
+      expect(items.at(1).element.style.getPropertyPriority('display')).toBe('important');
+      expect(more.element.style.display).toBe('');
 
       wrapper.destroy();
       window.ResizeObserver = NativeResizeObserver;
